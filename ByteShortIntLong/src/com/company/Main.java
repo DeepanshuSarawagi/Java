9fd249133db44667d91e390ac193b913e9702a61
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 22:24:57 +0530

    Learn about primitive data types in Java: Long

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -14,13 +14,13 @@ public class Main {
 		System.out.println("Busted Max Integer value: " + (myMaxIntValue + 1));
 		// As you can see the maximum Integer value is busted since the computer has already
 		// allocated the maximum integer value it can accept and we busted it by adding 1 to it
-
+		System.out.println();
 		byte myMinByteValue = Byte.MIN_VALUE;
 		byte myMaxByteValue = Byte.MAX_VALUE;
 
 		System.out.println("Mininum Byte Value: " + myMinByteValue);
 		System.out.println("Maximum Byte Value: " + myMaxByteValue);
-
+		System.out.println();
 		// Print the min and max short value in Java
 
 		short myMinShortValue = Short.MIN_VALUE;
@@ -28,5 +28,13 @@ public class Main {
 
 		System.out.println("Mininum Short Value: " + myMinShortValue);
 		System.out.println("Maximum Short Value: " + myMaxShortValue);
+		System.out.println();
+		// print the maximum and minimum long value in Java
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+
+		System.out.println("Mininum Long Value: " + myMinLongValue);
+		System.out.println("Maximum Long Value: " + myMaxLongValue);
+		
     }
 }
