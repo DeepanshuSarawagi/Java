6b74113e9266d476c73cd19c93973a932973245e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 21:23:09 +0530

    Define a variable and print the value
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
