a3ec6c21fb4567146ba729d75f0da0bff16434b0
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 21:36:34 +0530

    Evaluating expressions
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,8 +2,14 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
         int myFirstNumber = (10 + 5) + (10 * 2);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+
         System.out.println(myFirstNumber);
+        System.out.println(myTotalNumber);
     }
 }
 
