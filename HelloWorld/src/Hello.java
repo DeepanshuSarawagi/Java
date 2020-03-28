8831a0d940a0abe1cc493a033b20e69d22a13e0d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 21:28:28 +0530

    Evaluating expressions
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (10 * 2);
         System.out.println(myFirstNumber);
     }
 }
+
+// Any sequence of characters printed in double quotes is called string literal
