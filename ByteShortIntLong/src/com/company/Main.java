70458097a3821b30cb9eb2621b1f2495fa4d2899
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 22:11:42 +0530

    Learn about primitive data types in Java: Byte

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -14,5 +14,11 @@ public class Main {
 		System.out.println("Busted Max Integer value: " + (myMaxIntValue + 1));
 		// As you can see the maximum Integer value is busted since the computer has already
 		// allocated the maximum integer value it can accept and we busted it by adding 1 to it
+
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+
+		System.out.println("Mininum Byte Value: " + myMinByteValue);
+		System.out.println("Maximum Byte Value: " + myMaxByteValue);
     }
 }
