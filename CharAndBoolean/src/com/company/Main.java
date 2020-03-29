73f607c548828beca25dc5928d51f8867dd34c51
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 15:17:06 +0530

    Create new project to learn about char datatype

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -7,6 +7,13 @@ public class Main {
         char myChar = 'D';
         // char is yet another data type which is used to store single charater in single quotes
         System.out.println("The char is:  " + myChar);
+        // lets print the equivalent unicode value of character D
+        char myUnicodeCharValue = '\u0044';
+        System.out.println(myUnicodeCharValue);
+        char myOtherChar = '\u0926';
+        System.out.println(myOtherChar);
+
+        // \u stands for unicode. It should be a four digit unicode
 
     }
 }
