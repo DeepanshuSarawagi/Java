62d2061f671474fa6bc58087b4676c3ea1e3c4b0
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 16:07:10 +0530

    Learn about operators and operands in Java

---
+++ Operators/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("Previous result is = " + previousResult);
+
+    }
+}
