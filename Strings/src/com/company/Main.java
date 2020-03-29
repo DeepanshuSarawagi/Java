67a9a395b4702f7e30c995517205aed9ca7409e8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 15:49:26 +0530

    Learn about string datatype in Java

---
+++ Strings/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // byte
+        // int
+        // short
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        String myStringValue = "Deepanshu";
+        System.out.println(myStringValue);
+        System.out.println(myStringValue.length());
+
+    }
+}
