e0ebd5cf35f82002836658f18660bca8d11bfd7d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 12:55:43 +0530

    Learn about primitive data types: Float

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("The Minimum float value is: " + myMinFloatValue);
+        System.out.println("The Maximum float value is " + myMaxFloatValue);
+
+    }
+}
