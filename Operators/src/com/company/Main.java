c707bec97647de9a71ce2af4596664fa68e2d286
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 16:14:59 +0530

    Learn about operators and operands in Java

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -9,6 +9,13 @@ public class Main {
         System.out.println("1 + 2 = " + result);
         int previousResult = result;
         System.out.println("Previous result is = " + previousResult);
+        result -= 1;
+        System.out.println("Result's new value is: " + result);
+        result *= 10;
+        System.out.println("Result is now: " + result);
+
+        double resultDouble = (result / 3d);
+        System.out.println("Result is now: " + resultDouble);
 
     }
 }
