03efaf1465236fcf9f6896c0a41a68222f57d704
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 12:41:30 +0530

    Complete the basic Udemy challenge on primitive data types

---
+++ ByteShortIntLong/src/com/company/PrimitiveTypesChallenge.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class PrimitiveTypesChallenge {
+
+    public static void main(String[] args) {
+
+            byte byteVariable = 120;
+            short shortVariable = 2456;
+            int intVariable = 567823;
+            long longVariable = (50000 + (10 * (byteVariable + shortVariable + intVariable)));
+            System.out.println("The value of long number is " + longVariable);
+
+    }
+
+}
