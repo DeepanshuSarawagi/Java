ed8145048f2eb0aba198ddf5e62feae27d431120
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 15:51:36 +0530

    How to append two strings

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -15,6 +15,9 @@ public class Main {
         String myStringValue = "Deepanshu";
         System.out.println(myStringValue);
         System.out.println(myStringValue.length());
-
+        myStringValue = myStringValue + ", and I`m learning Java.";
+        System.out.println(myStringValue);
+        myStringValue = myStringValue + " \u00A9 2020.";
+        System.out.println(myStringValue);
     }
 }
