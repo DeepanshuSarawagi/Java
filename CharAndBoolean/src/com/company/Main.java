559b4de3121fe550c227846178e6cc1d53e3fe49
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-29 13:54:38 +0530

    Create new project to learn about char datatype

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        // char is yet another data type which is used to store single charater in single quotes
+        System.out.println("The char is:  " + myChar);
+
+    }
+}
