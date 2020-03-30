579d6f0dec665b58deadd887b54c21af332c9f97
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-30 17:43:25 +0530

    Learn how to create methods in Java

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -9,14 +9,24 @@ public class Main {
         int level = 5;
         int bonus = 100;
 
-        if (gameOver) {
-           int finalScore = level * bonus + score;
-            System.out.println("You final score is: " + finalScore);
-        }
+        calculateScore();
 
         score = 10000;
         level = 8;
         bonus = 200;
+        if (gameOver) {
+            int finalScore = level * bonus + score;
+            System.out.println("You final score is: " + finalScore);
+        }
+    }
+
+    public static void calculateScore() {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int level = 5;
+        int bonus = 100;
+
         if (gameOver) {
             int finalScore = level * bonus + score;
             System.out.println("You final score is: " + finalScore);
