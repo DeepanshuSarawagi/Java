94c83e5c0f69d597ba3cbb06295f9e4fc3f06034
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-30 17:33:05 +0530

    Create new project to learn about methods in Java

---
+++ Methods/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int level = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+           int finalScore = level * bonus + score;
+            System.out.println("You final score is: " + finalScore);
+        }
+
+        score = 10000;
+        level = 8;
+        bonus = 200;
+        if (gameOver) {
+            int finalScore = level * bonus + score;
+            System.out.println("You final score is: " + finalScore);
+        }
+    }
+}
