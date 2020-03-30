8c4020ae8350162918fafa3ece7e4758169dd47e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-30 16:33:58 +0530

    Learn about keywords and statements

---
+++ KeywordsAndExpression/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int highScore = 50;
+        if (highScore == 50){
+            System.out.println("Congratulations");
+        }
+	    int anotherVariable = 50;anotherVariable--;System.out.println("value of anotherVariable is: "
+                + anotherVariable);
+    }
+}
