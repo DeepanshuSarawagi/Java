d0c8ebe73ade9f35095e0a0f4cd459b846069f1c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-30 16:50:49 +0530

    Multiple if then else block statements

--- KeywordsAndExpression/ifKeywordAndCodeBlocks/src/ifKeyword.java
+++ KeywordsAndExpression/ifKeywordAndCodeBlocks/src/ifKeyword.java
@@ -6,8 +6,10 @@ public class ifKeyword {
         int score = 5000;
         int level = 5;
         int bonus = 100;
-        if(score < 5000) {
+        if(score < 5000 && score > 1000) {
             System.out.println("Your score is: " + score);
+        } else if (score < 1000){
+            System.out.println("Your score was less than 1000");
         }
         else {
             System.out.println("Got here without any hassle");
