c220a86b46f1e5c9077559cc8bbcd3233f6f5637
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-30 17:49:35 +0530

    avoid code duplication by passing arguments in methods and calling it

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,20 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 5000;
         int level = 5;
         int bonus = 100;
 
         calculateScore(true, 5000, level, bonus);
 
-        score = 10000;
-        level = 8;
-        bonus = 200;
-        if (gameOver) {
-            int finalScore = level * bonus + score;
-            System.out.println("You final score is: " + finalScore);
-        }
+        calculateScore(true, 10000, 8, 200);
     }
 
     public static void calculateScore(boolean gameOver, int score, int level, int bonus) {
