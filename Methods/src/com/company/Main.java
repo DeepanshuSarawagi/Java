63fa6053852bf4fb6643054ad0c1322a369e4f17
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-30 18:20:14 +0530

    Solved the basic problems on methods in Java

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -12,6 +12,26 @@ public class Main {
 
         highScore = calculateScore(true, 10000, 8, 200);
         System.out.println("Your final score is: " + highScore);
+
+        String playerName = "Deepanshu";
+        int playerScore = 1500;
+        int position = calculateHighScorePosition(playerScore);
+        displayHighScorePosition(playerName, position);
+
+        playerName = "Rajat";
+        playerScore = 900;
+        position = calculateHighScorePosition(playerScore);
+        displayHighScorePosition(playerName, position);
+
+        playerName = "Divya";
+        playerScore = 400;
+        position = calculateHighScorePosition(playerScore);
+        displayHighScorePosition(playerName, position);
+
+        playerName = "Pooja";
+        playerScore = 50;
+        position = calculateHighScorePosition(playerScore);
+        displayHighScorePosition(playerName, position);
     }
 
     public static int calculateScore(boolean gameOver, int score, int level, int bonus) {
@@ -24,5 +44,38 @@ public class Main {
             return finalScore;
         }
         return  -1;
+    }
+
+    // Udemy challenge
+    // Create a method called displayHighScorePosition
+    // it should a players name as a parameter, and a 2nd parameter as a position in the high score table
+    // You should display the players name along with a message like " managed to get into position " and the
+    // position they got and a further message " on the high score table".
+    //
+    // Create a 2nd method called calculateHighScorePosition
+    // it should be sent one argument only, the player score
+    // it should return an in
+    // the return data should be
+    // 1 if the score is >=1000
+    // 2 if the score is >=500 and < 1000
+    // 3 if the score is >=100 and < 500
+    // 4 in all other cases
+    // call both methods and display the results of the following
+    // a score of 1500, 900, 400 and 50
+    //
+
+    public static void displayHighScorePosition(String playerName, int position){
+        System.out.println("Player " + playerName + " managed to get into position " + position + " on the high " +
+                "score table");
+    }
+    public static int calculateHighScorePosition(int playerScore){
+        if (playerScore >=1000){
+            return 1;
+        } else if(playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if(playerScore >= 100 && playerScore < 500){
+            return 3;
+        }
+        return 4;
     }
 }
