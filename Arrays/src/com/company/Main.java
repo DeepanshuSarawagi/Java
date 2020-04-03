0da8fb74ea08d0ab90b532c5aec68448b713835b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-03 21:50:01 +0530

    Create new project to learn about Arrays in Java

---
+++ Arrays/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* In this chapter we are going to learn about Arrays in Java.
+	Array is nothing but a group of like-typed variables that are referred to by a common name. Arrays of any type can
+	be created and have one or more dimension.
+	 */
+    }
+}
