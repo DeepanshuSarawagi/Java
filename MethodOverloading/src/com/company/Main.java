976fc37b6f15717cd6a5ed04f4e20ff3bc8db297
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-04 20:03:44 +0530

    Learn about method overloading in Java

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("New score is " + newScore);
         int newScoreOfUnnamedPlayer = calculateScore(5000);
         System.out.println(newScoreOfUnnamedPlayer);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -14,8 +15,21 @@ public class Main {
         return score * 100;
     }
 
-    public static int calculateScore(int score) {
+    public static int calculateScore(int score) {  // overloading the method by using the same name but different params
         System.out.println("Unnamed Player " + "has scored " + score + " points");
         return score * 100;
+    }
+
+    public static void calculateScore() {  // overloading the method by using the same name but different params
+        System.out.println("No Player, No Score " );
+    }
+
+//    public static int calculateScore() {
+//        System.out.println("No Player, No Score ");
+//        return 0;
+        /* The code on line 27 to 29 will throw an error. We cannot overload an method by changing the datatype of the
+        return value of a method. To overload a method, we need to define unique parameters. This is called as changing
+        the signature of the method so that we can overload it.
+         */
     }
 }
