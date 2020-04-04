1ecced8b3ef0062e26e11d7f84d2effc33042dd3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-04 22:23:16 +0530

    Complete the Udemy challenge

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -8,6 +8,14 @@ public class Main {
         int newScoreOfUnnamedPlayer = calculateScore(5000);
         System.out.println(newScoreOfUnnamedPlayer);
         calculateScore();
+
+        System.out.println();
+
+        double cm = calcFeetAndInchesToCentimeters(10, 12);
+        System.out.println(cm);
+
+        double centimeters = calcFeetAndInchesToCentimeters(200);
+        System.out.println(centimeters);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -33,5 +41,29 @@ public class Main {
         the signature of the method so that we can overload it.
          */
 
-}
+        // Calculate feet and inches to centimeters challenge on Udemy
 
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if (feet >= 0){
+            if (inches >=0 && inches <=12) {
+                double feetToInches = feet * 12;
+                return (inches + feetToInches) * 2.54;
+            }
+            System.out.println("Invalid feet or inches");
+        }
+
+        return -1;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+
+        int feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " Inches" + " has " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+
+}
