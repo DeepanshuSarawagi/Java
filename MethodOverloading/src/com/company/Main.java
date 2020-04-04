7641504e400cdf4c9f642b43e9bc45cc12025b4d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-04 19:56:12 +0530

    Learn about method overloading in Java

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int newScore = calculateScore("Deepashu", 500);
+        System.out.println("New score is " + newScore);
+        int newScoreOfUnnamedPlayer = calculateScore(5000);
+        System.out.println(newScoreOfUnnamedPlayer);
+    }
 
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " has scored " + score + " points");
+        return score * 100;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed Player " + "has scored " + score + " points");
+        return score * 100;
     }
 }
