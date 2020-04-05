b7fe33c3f236d40fca7e695591a2bafd7b70c3e0
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-05 22:00:58 +0530

    Solved the Udemy coding exercise 11: isPlaying cat

---
+++ CodingExercises/PlayingCat/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(PlayingCat.isCatPlaying(true, 26));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+        System.out.println(PlayingCat.isCatPlaying(true, 39));
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 10));
+    }
+}
