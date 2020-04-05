73be15c25a49317441f2f7a76ae70160972fae08
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-05 17:26:06 +0530

    Solve the Udemy challenge on converting Minutes and seconds to Hours, minutes and seconds

--- CodingExercises/SecondsAndMinutesChallenge/src/com/company/Main.java
+++ CodingExercises/SecondsAndMinutesChallenge/src/com/company/Main.java
@@ -3,6 +3,52 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String hoursMinutesAndSeconds = getDurationString(300, 0);
+        System.out.println(hoursMinutesAndSeconds);
+        hoursMinutesAndSeconds = getDurationString(3689);
+        System.out.println(hoursMinutesAndSeconds);
+
+        }
+
+    public static String getDurationString(int minutes, int seconds){
+
+        int hours = 0;
+
+        if ((minutes < 0) || (seconds < 0 || seconds > 59)){
+            return "Invalid Value";
+        }
+
+        if (minutes > 59) {
+            hours = minutes / 60;
+            minutes = minutes % 60;
+
+            if (hours < 10){
+                if (minutes < 10){
+                    if (seconds < 10){
+                        return "0" + hours + "h " + "0" + minutes + "m " + "0" + seconds + "s";
+                    }
+                    return "0" + hours + "h " + "0" + minutes + "m " + seconds + "s";
+                }
+                return "0" + hours + "h " + minutes + "m " + seconds + "s";
+            }
+        }
+        return hours + "h " + minutes + "m " + seconds + "s";
+
+    }
+
+    public static String getDurationString(int seconds){
+
+        int minutes;
+        if (seconds < 0) {
+            return "Invalid Value";
+        }
+
+        minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+        String minutesHoursAndSeconds = getDurationString(minutes, remainingSeconds);
+        System.out.println(minutesHoursAndSeconds);
+        return minutesHoursAndSeconds;
+
     }
 }
