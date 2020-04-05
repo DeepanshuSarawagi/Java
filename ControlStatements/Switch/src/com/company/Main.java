a884247cdaa505d9fa038e402b6f334267cc32f1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-05 22:25:11 +0530

    Multiple cases in single folding

--- ControlStatements/Switch/src/com/company/Main.java
+++ ControlStatements/Switch/src/com/company/Main.java
@@ -27,6 +27,10 @@ public class Main {
             case 2:
                 System.out.println("Value was 2");
                 break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3 or a 4 or 5");
+                System.out.println("Well, it was actually a " + switchValue);
+                break;
             default:
                 System.out.println("Value was not 1 or 2");
                 break;
