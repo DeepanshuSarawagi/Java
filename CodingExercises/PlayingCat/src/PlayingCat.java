71ba65eef371261d16fe629b7f4a8eab73ae1bfd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-05 22:03:13 +0530

    Simplify the return statement in the Udemy coding exercise 11: isPlaying cat

--- CodingExercises/PlayingCat/src/PlayingCat.java
+++ CodingExercises/PlayingCat/src/PlayingCat.java
@@ -33,9 +33,6 @@ public class PlayingCat {
         if (summer && (temperature >= 25 && temperature <= 45)){
             return true;
         }
-        if (!summer && (temperature >= 25 && temperature <= 35)){
-            return true;
-        }
-        return false;
+        return !summer && (temperature >= 25 && temperature <= 35);
     }
 }
