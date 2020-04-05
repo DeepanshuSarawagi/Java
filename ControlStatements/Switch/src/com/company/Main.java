39f96fb578a1928152e785985256e7ad68045b27
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-05 22:21:07 +0530

    Basic code using switch statement

--- ControlStatements/Switch/src/com/company/Main.java
+++ ControlStatements/Switch/src/com/company/Main.java
@@ -3,6 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // code using if statement
+
+        int value = 3;
+        if (value == 1){
+            System.out.println("Value was 1");
+        } else if (value ==2){
+            System.out.println("Value was 2");
+        }
+        else {
+            System.out.println("Value was not 1 or 2");
+        }
+        System.out.println("************************");
+        // Equivalent code using switch statement
+
+        int switchValue = 3;
+
+        switch(switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 or 2");
+                break;
+
+        }
     }
 }
