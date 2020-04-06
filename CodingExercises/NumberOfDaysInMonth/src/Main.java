d91e39a7a2c8020be97227122f526c44ac0f4262
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-06 17:30:02 +0530

    Solved coding exercise 12: NumberOfDaysInMonth

---
+++ CodingExercises/NumberOfDaysInMonth/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(NumberOfDaysInMonth.isLeapYear(16));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 500));
+    }
+}
--- CodingExercises/NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ CodingExercises/NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -1,4 +1,4 @@
-/*Write a method isLeapYear with a parameter of type int named year.
+/* Write a method isLeapYear with a parameter of type int named year.
 
 The parameter needs to be greater than or equal to 1 and less than or equal to 9999.
 
@@ -56,5 +56,42 @@ NOTE: Do not add a main method to solution code.
 
 public class NumberOfDaysInMonth {
 
-    public static boolean
-}
+    public static boolean isLeapYear(int year) {
+
+        if (year <= 0 || year > 9999) {
+            return false;
+        } else {
+            if (year % 4 == 0) {
+                return year % 100 != 0 || year % 400 == 0;
+            }
+            return false;
+        }
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+
+        System.out.println(isLeapYear(year));
+
+        if (month > 0 && month < 13){
+            if (year > 0 && year < 10000){
+                    switch (month) {
+                        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                            return 31;
+                        case 2:
+                            if(isLeapYear(year)) {
+                                return 29;
+                            }else {
+                                return 28;
+                            }
+                        case 4: case 6: case 9: case 11:
+                            return 30;
+                        default:
+                            return -1;
+                    }
+                }
+            return -1;
+            }
+        return -1;
+        }
+    }
+
