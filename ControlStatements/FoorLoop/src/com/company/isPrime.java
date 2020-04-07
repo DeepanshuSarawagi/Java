bed064c919bb09e636e98e0b2dfd4e998a7f65c2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-07 11:54:51 +0530

    Udemy challenge: Use a controlled loop to calculate the first three prime numbers in any given range

---
+++ ControlStatements/FoorLoop/src/com/company/isPrime.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class isPrime {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPrimeNumber(20));
+        System.out.println("******************");
+        int count = 1;
+        for (int i = 578; i < 1000; i++) {
+                if (isPrimeNumber(i)) {
+                    count ++;
+                    System.out.println("Prime number is: " + i);
+                }
+                if (count > 3) {
+                    break;
+                }
+            }
+        }
+
+    public static boolean isPrimeNumber(int n){
+
+        if (n == 1){
+            return false;
+        }
+
+        for (int i =2; i <= n/2; i++){
+            if(n % i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
