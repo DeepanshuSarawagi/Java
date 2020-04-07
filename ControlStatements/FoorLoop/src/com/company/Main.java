69ac07348386905dc62629e1e6b08c453238e09e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-07 10:46:30 +0530

    Basic for loop in Java

--- ControlStatements/FoorLoop/src/com/company/Main.java
+++ ControlStatements/FoorLoop/src/com/company/Main.java
@@ -7,6 +7,16 @@ public class Main {
 
         for (int i =0; i < 6; i++){
             System.out.println("Loop " + i + " hello");
+
         }
+
+        for (double i = 2; i < 9; i++){
+            System.out.println(i + " % Interest on amount 10000 is " + calculateInterestRate(10000.0, i));
+        }
+
+        }
+
+    public static double calculateInterestRate(double amount, double interestRate){
+        return (amount * (interestRate / 100));
     }
 }
