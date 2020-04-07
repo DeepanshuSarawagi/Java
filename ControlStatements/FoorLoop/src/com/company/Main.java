239d9194f291b8d5a4ba67c758208820d1608be1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-07 10:35:43 +0530

    Basic for loop in Java

---
+++ ControlStatements/FoorLoop/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Basic for loop
+
+        for (int i =0; i < 6; i++){
+            System.out.println("Loop " + i + " hello");
+        }
+    }
+}
