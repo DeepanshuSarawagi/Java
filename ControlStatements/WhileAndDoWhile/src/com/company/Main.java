f1368cf697ecf5497b77159f025d44b0e0d46f74
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-08 11:54:50 +0530

    A simple do while loop

--- ControlStatements/WhileAndDoWhile/src/com/company/Main.java
+++ ControlStatements/WhileAndDoWhile/src/com/company/Main.java
@@ -8,5 +8,24 @@ public class Main {
             System.out.println("Count is now: " + count);
             count++;
         }
+
+	    // Alternative way to define a while loop
+
+        count = 1; //set the counter back to 1 after above code is executed
+        while (true) {
+            if(count == 6){
+                break;
+            }
+                System.out.println("Count is now: " + count);
+                count++;
+            }
+
+        // Alternative way of defining while loop using do while. In this case code will execute at least once.
+        count = 1; // resetting the counter back to 1
+        System.out.println("************************");
+        do {
+            System.out.println("Count is now: " + count);
+            count ++;
+        }while (count != 6);
+        }
     }
-}
