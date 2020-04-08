3556b6183e43410dd6944b6542be4c1d205c5a91
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-08 12:54:56 +0530

    Create new project to work on sum digits challenge

---
+++ CodingExercises/DigitSumChallenge/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static int sumDigits(int n){
+        if (n <= 9){
+            return -1;
+        }
+        else {
+
+        }
+    }
+}
