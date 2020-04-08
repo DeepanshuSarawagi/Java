732b4689ddd2317d3c3e7d7bab8b00c3fd00369b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-08 13:32:21 +0530

    Solve the Digit Sum challenge- remove the TODO items

--- CodingExercises/DigitSumChallenge/src/com/company/Main.java
+++ CodingExercises/DigitSumChallenge/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sumDigits(87346923));
+        System.out.println(sumDigits(99999));
     }
 
     public static int sumDigits(int n) {
@@ -14,19 +14,13 @@ public class Main {
             return -1;
         } else {
             remainingDigits = n / 10;
-            System.out.println("Remaining digits are now: " + remainingDigits);  // TODO remove this line after testing
             lastDigit = n % 10;
-            System.out.println("Last digit is: " + lastDigit); // TODO remove this line after testing
             sumOfDigits = sumOfDigits + lastDigit;
-            System.out.println("Sum of digits is now: " + sumOfDigits); // TODO remove this line after testing
             while (true) {
                 if ((remainingDigits != 0)) {
                     lastDigit = remainingDigits % 10;
                     remainingDigits = remainingDigits / 10;
-                    System.out.println("Last digit is now: " + lastDigit);  // TODO remove this line after testing
-                    System.out.println("remaining digits are now: " + remainingDigits); // TODO remove this line after testing
                     sumOfDigits = sumOfDigits + lastDigit;
-                    System.out.println("Sum of digits is now: " + sumOfDigits); // TODO remove this line after testing
                 }
                 else {
                     break;
