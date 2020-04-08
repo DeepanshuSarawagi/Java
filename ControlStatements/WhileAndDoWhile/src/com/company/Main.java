7aeade7c7463a8c72ea61fbbc4456a100d95c011
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-08 11:40:00 +0530

    A simple while loop

--- ControlStatements/WhileAndDoWhile/src/com/company/Main.java
+++ ControlStatements/WhileAndDoWhile/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int count = 0;
+	    while (count != 6){
+            System.out.println("Count is now: " + count);
+            count++;
+        }
     }
 }
