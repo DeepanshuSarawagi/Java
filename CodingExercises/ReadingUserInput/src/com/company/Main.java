995c4458ace250d5ad14c5b3529c51a10fd19d36
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-11 23:12:47 +0530

    Reading input challenge

--- CodingExercises/ReadingUserInput/src/com/company/Main.java
+++ CodingExercises/ReadingUserInput/src/com/company/Main.java
@@ -17,11 +17,21 @@ public class Main {
                 System.out.println("Entered #" + count + " is: " + number);
                 sum += number;
                 count++;
+                System.out.println("Please enter a number of your choice: ");
+                scanner.hasNextInt();
             }
-        } else {
+        }
+        else {
             System.out.println("Please enter a valid number: ");
             scanner.nextLine();
-            number = scanner.nextInt();
+            while (count < 11) {
+                number = scanner.nextInt();
+                System.out.println("Entered #" + count + " is: " + number);
+                sum += number;
+                count++;
+                System.out.println("Please enter a number of your choice: ");
+                scanner.hasNextInt();
+            }
         }
         System.out.println("Sum of 10 entered number is: " + sum);
     }
