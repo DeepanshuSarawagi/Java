62fad8c822fa49458c18cb6d63cb30c6112f1e62
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-12 17:05:56 +0530

    How to get the data of field using getter

--- ObjectOrientedProgramming/Classes/src/com/company/Main.java
+++ ObjectOrientedProgramming/Classes/src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Car porsche = new Car();  // Creating new object of class Car and initialize it.
         Car holden = new Car();
+        System.out.println("Model is " + porsche.getModel());
         porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
     }
 }
