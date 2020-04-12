05862659f430ccdd29f915a251bde1a386ba3476
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-12 16:51:37 +0530

    Learn how to create classes and objects of those classes

---
+++ ObjectOrientedProgramming/Classes/src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+
+    // creating variables in classes. These are called as fields of classes.
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+}
