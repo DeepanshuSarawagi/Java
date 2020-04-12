d28b6638c1ea34d43b400287739fdcd946e85e77
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-12 18:07:22 +0530

    Solved Udemy coding exercise 30: Person

---
+++ CodingExercises/Person/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("fullname= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("Deepanshu");
+        person.setAge(18);
+        System.out.println("fullname= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Sarawagi");
+        System.out.println("fullname= " + person.getFullName());
+    }
+}
