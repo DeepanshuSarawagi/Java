38b51b90d8f6e91709c6eefcbfb15ed92337ccbb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-14 15:48:56 +0530

    Define accelerate method in Outlander class

--- CodingExercises/InheritanceChallenge/src/com/company/Outlander.java
+++ CodingExercises/InheritanceChallenge/src/com/company/Outlander.java
@@ -2,5 +2,39 @@ package com.company;
 
 public class Outlander extends Car {
 
+    private int roadServiceMethods;
 
+    public Outlander(int roadServiceMethods) {
+        super("Outlander", "4WD", 5, 5, 6, false);
+        this.roadServiceMethods = roadServiceMethods;
+    }
+
+    public void accelerate(int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if (newVelocity == 0) {
+            stop();
+        }
+        else if (newVelocity > 0 && newVelocity <=10) {
+            changeGear(1);
+        }
+        else if (newVelocity > 10 && newVelocity <=20) {
+            changeGear(2);
+        }
+        else if (newVelocity >20 && newVelocity <=30) {
+            changeGear(3);
+        }
+        else if (newVelocity > 30 && newVelocity <=40) {
+            changeGear(4);
+        }
+        else if (newVelocity > 40 && newVelocity <=60) {
+            changeGear(5);
+        }
+        else {
+            changeGear(6);
+        }
+
+        if (newVelocity > 0) {
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
 }
