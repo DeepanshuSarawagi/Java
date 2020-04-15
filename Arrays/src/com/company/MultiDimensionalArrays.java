0349531ab060cbb43175f8baa5b0b73854b3a02d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-15 22:34:05 +0530

    Create ThreeDimensionalArray

--- Arrays/src/com/company/MultiDimensionalArrays.java
+++ Arrays/src/com/company/MultiDimensionalArrays.java
@@ -6,6 +6,7 @@ public class MultiDimensionalArrays {
         twoDimensionalArray();
         TwoDAgain.twoDAgainUnequal();
         Matrix.MatrixArray();
+        ThreeDimensionalArray.ThreeDArray();
     }
 
     public static void twoDimensionalArray() {
@@ -78,5 +79,34 @@ class Matrix {
             }
             System.out.println();
         }
+        System.out.println();
+    }
+}
+
+class ThreeDimensionalArray {
+    public static void ThreeDArray() {
+
+        int i, j, k = 0;
+
+        int[][][] threeD = new int[3][4][5];
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < 5; k++){
+                    threeD[i][j][k] = i * j * k;
+                }
+            }
+        }
+
+        for (i = 0; i < 3; i++) {
+            System.out.println("When i is " + i);
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < 5; k++) {
+                    System.out.print(threeD[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+        }
+
     }
 }
