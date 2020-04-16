626d7f47f158c4c5528e880871717e9d866b247c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-16 12:30:36 +0530

    Create new project to learn more about Polymorphism

---
+++ ObjectOrientedProgramming/PolymorphismChallenge/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
