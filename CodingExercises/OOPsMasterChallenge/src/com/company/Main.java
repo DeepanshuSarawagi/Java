2abb3dfbf8eb7cf3b1e63553bb532d9bbbe3b366
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-18 13:35:15 +0530

    Allow user to add additional items in Hamburger

--- CodingExercises/OOPsMasterChallenge/src/com/company/Main.java
+++ CodingExercises/OOPsMasterChallenge/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,10 +10,19 @@ public class Main {
         System.out.println(hamburger.getBasePrice());
 //        hamburger.AddAdditionalItems(2);
         int count = 1;
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please select the items to be added in the burger.");
         while (count < 5) {
-            hamburger.AddAdditionalItems(count);
-            count++;
+            System.out.println("Please enter a number between 1-10 to add an addtional item in burger: ");
+            boolean hasNextInt = scanner.hasNextInt();
+            if (hasNextInt) {
+                int number = scanner.nextInt();
+                hamburger.AddAdditionalItems(number);
+                count++;
+            } else {
+                System.out.println("Invalid input");
+            }
+            scanner.nextLine();
         }
         System.out.println("Your Hamburger is ready and final price is " + hamburger.HamBurgerFinalPrice());
     }
