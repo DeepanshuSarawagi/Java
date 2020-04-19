5c0b54135ab1bc7f50e62bf9a46512830b29f5d4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-19 20:41:04 +0530

    Define methods to add, modify, find, remove an item from ArrayList

---
+++ ListAndArrayList/src/com/company/GroceryList.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<>();  // define an ArrayList
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);  // use the ArrayList.add() method to add an item in the ArrayList
+    }
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size());
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String item) {
+        groceryList.set(position, item);
+        System.out.println("Grocery item " + (position + 1) + " was modified to " + groceryList.get(position));
+    }
+
+    public void removeGroceryItem(int position) {
+        String item = groceryList.get(position);
+        groceryList.remove(item);
+    }
+
+    public String findItem(String searchItem) {
+        int position = groceryList.indexOf(searchItem);
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+        return null;
+    }
+}
--- ListAndArrayList/src/com/company/Main.java
+++ ListAndArrayList/src/com/company/Main.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
 
     public static void main(String[] args) {
 
