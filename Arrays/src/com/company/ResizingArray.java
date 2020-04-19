bbb411f8dcca225ea497025ee5616fd70ab2e4d4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-19 19:47:38 +0530

    Learn how to resize an Array

--- Arrays/src/com/company/ResizingArray.java
+++ Arrays/src/com/company/ResizingArray.java
@@ -23,8 +23,8 @@ public class ResizingArray {
     }
 
     private static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int value : array) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
