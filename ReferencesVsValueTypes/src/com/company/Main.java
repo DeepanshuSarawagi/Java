41be0a1aed2fdf216a6501f0074a0dfdc86b2da2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-19 15:19:34 +0530

    Learn the difference of ReferencesVsValueTypes

---
+++ ReferencesVsValueTypes/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myIntValue = 10;
+	    int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        // References value types example
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray = " + Arrays.toString(anotherArray));
+
+    }
+}
