9a85d4d008d5da72b1823052ba08fd3e82d4e440
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-19 17:41:06 +0530

    Print both reversed and non-reversed array

--- CodingExercises/ReverseArrayChallenge/src/com/company/Main.java
+++ CodingExercises/ReverseArrayChallenge/src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int[] myArray = new int[] {1, 2, 3, 4, 5};
+	    int[] myArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	    for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Element " + i + " is " + myArray[i]);
+        }
+        System.out.println();
+
         reverse(myArray);
     }
 
