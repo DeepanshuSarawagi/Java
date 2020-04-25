57fa3113c21a00d9d6b6ef83888693ef95b610c3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-25 18:00:48 +0530

    Completed the Bank, Branch class.

--- CodingExercises/AutoboxingAndUnboxingChallenge/src/com/company/Main.java
+++ CodingExercises/AutoboxingAndUnboxingChallenge/src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Bank bank = new Bank("ICICI");
+        bank.addBranch("Chennai");
+        bank.addCustomer("Chennai", "Deepanshu", 145.2);
+        bank.addCustomer("Chennai", "Divya", 345.12);
+        bank.addCustomer("Chennai", "Rajat", 324.34);
+        bank.addBranch("Pune");
+        bank.addCustomer("Pune", "Raj", 1234.543);
+        bank.addCustomerTransaction("Chennai", "Deepanshu", 123.3);
+        bank.addCustomerTransaction("Chennai", "Divya", 32.45);
+        bank.addCustomerTransaction("Pune", "Raj", 231.34);
+        bank.listCustomers("Chennai", false);
+        bank.listCustomers("Chennai", true);
+
+
     }
 }
