a29d9d4c762cf732c5b163986809641d7c02ab66
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-26 13:08:28 +0530

    Understand how we need to use LinkedList and ListIterator

--- LinkedList/src/com/company/Demo.java
+++ LinkedList/src/com/company/Demo.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
@@ -30,6 +31,7 @@ public class Demo {
         addInOrder(placesToVisit, "Agra");
         printList(placesToVisit);
         addInOrder(placesToVisit, "Chennai");
+        visit(placesToVisit);
     }
 
     private static void printList(LinkedList<String> linkedList) {
@@ -64,5 +66,69 @@ public class Demo {
         }
         stringListIterator.add(newCity);
         return true;
+    }
+
+    private static void visit(LinkedList<String> cities) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if (cities.isEmpty()) {
+            System.out.println("No cities in your itinerary");
+            return;
+        } else {
+            System.out.println("Now visiting: " + listIterator.next());
+            printMenu();
+        }
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Vacation over");
+                    quit = true;
+                    break;
+                case 1:
+                    if (!goingForward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now visiting: " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the list");
+                        goingForward = false;
+                    }
+                    break;
+                case 2:
+                    if (goingForward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now Visiting: " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                        goingForward = true;
+                    }
+                    break;
+                case 3:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions: \nPress ");
+        System.out.println("0- To quit the vacation" + "\n" +
+                "1- To go to the next city" + "\n" +
+                "2- To go to the previous city" + "\n" +
+                "3- To print the menu options");
     }
 }
