0493303c23e1af52c9c4458ca7bff247f8a62e4c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-27 19:30:11 +0530

    Create instances of class DeskPhone and test functionality

--- Interfaces/src/com/company/Main.java
+++ Interfaces/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        ITelephone deepsPhone;
+        deepsPhone = new DeskPhone(89456357);
+        deepsPhone.callPhone(89456357);
+        System.out.println(deepsPhone.isRinging());
+        deepsPhone.answer();
+        deepsPhone.dial(3243553);
+        System.out.println(deepsPhone.isRinging());
     }
 }
