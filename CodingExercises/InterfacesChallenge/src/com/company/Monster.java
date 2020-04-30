b10dc2bc9b37f15645fe41889f6400e34d9c3c50
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-04-30 19:17:55 +0530

    Create new monster class implementing the ISaveable interface

---
+++ CodingExercises/InterfacesChallenge/src/com/company/Monster.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable{
+    private String name;
+    private int hitPoints;
+    private int Strength;
+
+    public Monster(String name, int hitPOints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPOints;
+        Strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPOints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return Strength;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPOints=" + hitPoints +
+                ", Strength=" + Strength +
+                '}';
+    }
+
+    @Override
+    public List<String> writeData() {
+        ArrayList<String> values = new ArrayList<>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.Strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.Strength = Integer.parseInt(savedValues.get(2));
+        }
+    }
+}
