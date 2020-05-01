339e5147011cb8f24764b7d46ebcc44a7606ad9d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-01 17:14:16 +0530

    Add the addGear method in GearBox class and not in Main class

--- InnerClasses/src/com/company/Main.java
+++ InnerClasses/src/com/company/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         GearBox mcLaren = new GearBox(6);
-        GearBox.Gear first = mcLaren.new Gear(1, 12.3);
-//        GearBox.Gear second = new GearBox.Gear(2, 15.6);  // this will not work. Incorrect syntax
-//        GearBox.Gear third = new mcLaren.Gear(3, 20.5);   // this will not work. Incorrect syntax
+//        GearBox.Gear first = mcLaren.new Gear(1, 12.3);
+////        GearBox.Gear second = new GearBox.Gear(2, 15.6);  // this will not work. Incorrect syntax
+////        GearBox.Gear third = new mcLaren.Gear(3, 20.5);   // this will not work. Incorrect syntax
+//
+//        System.out.println(first.driveSpeed(1000));
 
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(3);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(5);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(5000));
     }
 }
