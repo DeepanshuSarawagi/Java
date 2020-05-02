ba2eb6a09f3fff1b0b81380f0c47f6d75d64d98f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-02 17:18:14 +0530

    Modify existing project to create new solution using InnerClass

---
+++ CodingExercises/InnerClassChallenge/src/com/company/Main.java
@@ -0,0 +1,212 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Main {
+
+    private static ArrayList<Album> albums = new ArrayList<>();
+
+    public static void main(String[] args) {
+	    Album album = new Album("Strom Bringer", "Deep Purple");
+	    album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+
+        albums.add(album);
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Let's go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        album.addSong("Evil walks", 3.45);
+        album.addSong("C.O.D", 5.25);
+        album.addSong("Breaking rules", 5.32);
+        album.addSong("Nibht of the long knives", 5.12);
+
+        albums.add(album);
+
+        LinkedList<Song> playList = new LinkedList<>();
+        albums.get(0).addToPlaylist("You can't do it right", playList);
+        albums.get(0).addToPlaylist("Holy man", playList);
+        albums.get(0).addToPlaylist("Speed king", playList); // Doesn't exit
+        albums.get(0).addToPlaylist(9, playList);
+        albums.get(1).addToPlaylist(8, playList);
+        albums.get(1).addToPlaylist(6, playList);
+        albums.get(1).addToPlaylist(3, playList);
+        albums.get(1).addToPlaylist(24, playList); // Does not exist
+
+        play(playList);
+
+    }
+
+    private static void play(LinkedList<Song> playList) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean forward = true;
+        ListIterator<Song> listIterator = playList.listIterator();
+//        if (playList.size() == 0) {
+//            System.out.println("No songs in the playlist");
+//            return;
+//        } else {
+//            System.out.println("Now playing " + listIterator.next().toString());
+//        }
+        printMenu();
+        while (!quit) {
+            System.out.println("Please choose the option from the menu: \r");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Playlist complete");
+                    quit = true;
+                    break;
+                case 1:
+                    if (!forward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    } else {
+                        System.out.println("We have reached the end of the list");
+                        forward = false;
+                    }
+                    break;
+                case 2:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    } else {
+                        System.out.println("We are at the start of the playlist");
+                        forward = true;
+                    }
+                    break;
+                case 3:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We are at the start of the list");
+                        }
+                    } else {
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now replaying " + listIterator.next());
+                            forward = true;
+                        } else {
+                            System.out.println("We are at the end of the list");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playList);
+                    break;
+                case 5:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Please press: " + "\n" +
+                "0- To exit from the playlist" + "\n" +
+                "1- To play the next song from the play list" + "\n" +
+                "2- To play previous song from thr play list" + "\n" +
+                "3- To repeat the current song " + "\n" +
+                "4- To print the current playlist" + "\n" +
+                "5- To print the option menu");
+    }
+
+    private static void printList(LinkedList<Song> playList) {
+        System.out.println("Current Playlist: ");
+        for (int i=0; i<playList.size(); i++) {
+            System.out.println((i+1) + ". " + playList.get(i).toString());
+        }
+    }
+}
+
+class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            this.songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song song : this.songs) {
+            if (song.getTitle().equals(title)) {
+                return song;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if ((index >=0) && (index < this.songs.size())) {
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This song doesnt have the track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlaylist(String title, LinkedList<Song> playList) {
+        Song song = findSong(title);
+        if (song != null) {
+            playList.add(song);
+            return true;
+        }
+        System.out.println("Song " + title + " is not in the album");
+        return false;
+    }
+}
+
+class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
