563c6d32f31a3bad0fe8bb7b9e045c0d2a891c53
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-02 18:36:03 +0530

    Learn about postfix increment/decrement in Java
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Operators/src/com/company/IncDec.java
+++ Operators/src/com/company/IncDec.java
@@ -18,5 +18,20 @@ public class IncDec {
         System.out.println("x is now: " + x);
         System.out.println("y is now: " + y);
 
+        // Now let's consider below example with respect to postfix increment.
+
+        x = 42;
+        y = x++;
+        System.out.println("x is now: " + x);
+        System.out.println("y is now: " + y);
+
+        // This is similar to below lines of code
+        // first b is assigned to a and then a is incremented by 1
+        // y = x;
+        // x = x+ 1;
+
+        // in both the cases, be it prefix or postfix increment, x is incremented to 43 but the value of y depends on
+        // the type of increment.
+
     }
 }
