c9e1ef3b7e5e2c633dc9cc41ec4fa2cb7745c96f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-03 22:34:49 +0530

    Revisit switchCase statement in Java

--- ControlStatements/Switch/src/com/company/switchCase.java
+++ ControlStatements/Switch/src/com/company/switchCase.java
@@ -21,5 +21,32 @@ public class switchCase {
                     break;
             }
         }
+        // It is best practice to use break statements after each case to terminate the sequence and then the code
+        // execution moves to the first line of switch block. This is called as "jumping" out of sequence
+
+        // However, is some case, it may be required that multiple cases fall under single break. That is, you can omit
+        // break statement after each case. Look for below example
+
+        for (int i =0; i<=12; i++) {
+            switch (i) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println("i is less than 5");
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    case 9:
+                        System.out.println("i is less than 10");
+                        break;
+                default:
+                    System.out.println("i is 10 or greater");
+                    break;
+            }
+        }
     }
 }
