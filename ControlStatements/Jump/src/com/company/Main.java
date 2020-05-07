51030a4f7216e08b0e8c2a4ada367c8b8f0e9f17
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-07 22:31:27 +0530

    Learn how to use break as goto in Java

--- ControlStatements/Jump/src/com/company/Main.java
+++ ControlStatements/Jump/src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
         }
 
         // While using break, it breaks out of only the innermost loop if used in a nested loop.
-        // Look at the preceeding example for better understanding.
+        // Look at the preceding example for better understanding.
 
         for (int i = 0; i < 4; i++) {
             System.out.print("Pass " + i + ": ");
@@ -29,5 +29,32 @@ public class Main {
             }
             System.out.println();
         }
+
+        /*
+        break statement can also be used as a civilized form of GOTO statement in Java. Although, unlike other languages
+        Java doesn't doesn`t have a GOTO keyword, break can be used as a GOTO form. This block of code need not be any
+        switch or other loops, they can be any form of labeled block of code. Label is the block of code that identifies
+        name of the block. The labeled block must enclose the break statement. This should not be a immediate block
+        of code, instead it can be used in any block of nested blocks of code. When using the break in a nested block of
+        code, you can resume the execution of code from a certain block, you can cannot use it to transfer control out of
+        block.
+        To name a block, put a lable at start of it. Then use this lable as target block with break statement.
+        Consider the below example
+         */
+        boolean t = true;
+        first: {
+            second: {
+            third: {
+                System.out.println("Before the break");
+                if (t) {
+                    break second;
+                }
+                System.out.println("this won't execute");
+            }
+                System.out.println("This wont execute either");
+            }
+            System.out.println("This will execute");
+        }
+
     }
 }
