56155e7d411949e92004926b6ec03c5c647da585
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-09 20:11:42 +0530

    Understand the difference between call by value and call by reference

---
+++ ObjectOrientedProgramming/ArgumentPassing/src/callByValue.java
@@ -0,0 +1,29 @@
+public class callByValue {
+    public static void main(String[] args) {
+        Test ob = new Test();
+        int a = 15, b = 20;
+
+        System.out.println("Value of a and b before call: " + a + " " + b);
+        ob.meth(a, b);
+        System.out.println("Value of a and b after call: " + a + " " + b);
+
+        // once you run the program, you will see from the output that value of a and b remains same when arguments are
+        // passed to a sub-routine using call-by-value method
+    }
+}
+
+class Test{
+    // There are two ways in which a computer language can pass an argument to the sub-routine. call-by-value and
+    // call-by-reference.
+    // in call-by-value, this approach copies the value of argument into the formal parameter of the sub-routine.
+    // Therefore changes made to the parameter of the sub-routine has no effect on the argument passed.
+
+    // when you pass a primitive type to a method, it is passed as value. Thus a value of the argument is made and what
+    // occurs to the paramter that receives the arguments has no effect outside the method. For example, consider the
+    // following program
+
+    void meth(int i, int j) {
+        i *= 2;
+        j /= 2;
+    }
+}
