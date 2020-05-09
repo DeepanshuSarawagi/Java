ef4e8c2c3cf3b7f0e6b09215cbd2c4b9f7177081
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-09 21:59:48 +0530

    Returning objects as reference

--- ObjectOrientedProgramming/ReturningObjects/src/com/company/Main.java
+++ ObjectOrientedProgramming/ReturningObjects/src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Test test = new Test(2);
+        System.out.println("Value of a is " + test.getA());
+        Test test2;
+        test2 = test.incrmntByTen();
+        System.out.println("Value of a is " + test2.getA());
+        test2 = test2.incrmntByTen();
+        System.out.println("Value of a is " + test2.getA());
+
+        // As you can see, each time incrmntByTen() is called, a new object is created and a reference to that is
+        // returned to the calling routine.
+    }
+}
+
+class Test{
+    int a;
+
+    Test(int i) {
+        this.a = i;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    Test incrmntByTen() {
+
+        Test test = new Test(a+10);
+        return test;
     }
 }
