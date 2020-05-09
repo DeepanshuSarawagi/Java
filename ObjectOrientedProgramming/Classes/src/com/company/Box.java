a304a3df7a5e9773a43b9e5f847dcffb8423e820
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-09 15:55:18 +0530

    revisit basic`s of classes

--- ObjectOrientedProgramming/Classes/src/com/company/Box.java
+++ ObjectOrientedProgramming/Classes/src/com/company/Box.java
@@ -4,13 +4,18 @@ public class Box {
     double width;
     double height;
     double depth;
+
+    // create method to display volume of the box
+    void volume() {
+        System.out.println("Volume of the box is: " + (width*height*depth));
+    }
 }
 
 class BoxDemo {
     public static void main(String[] args) {
         Box myBox1 = new Box();
         Box myBox2 = new Box();
-        double volume;
+//        double volume;
 
         // assign value to myBox1`s instance variables
         myBox1.width = 10;
@@ -27,12 +32,16 @@ class BoxDemo {
 
         // compute volume of first box
 
-        volume = myBox1.width * myBox1.height * myBox1.depth;
-        System.out.println("Volume of myBox1 is: " + volume);
+//        volume = myBox1.width * myBox1.height * myBox1.depth;
+//        System.out.println("Volume of myBox1 is: " + volume);
+
+        myBox1.volume();
 
         // compute volume of second box
 
-        volume = myBox2.width * myBox2.height * myBox2.depth;
-        System.out.println("Volume of myBox2 is: " + volume);
+//        volume = myBox2.width * myBox2.height * myBox2.depth;
+//        System.out.println("Volume of myBox2 is: " + volume);
+        myBox2.volume();
+
     }
 }
