09a1d0b0e32f331a5d16f05966b814716eb17b51
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-09 19:49:08 +0530

    Learn how to pass objects of class as parameters in methods

--- ObjectOrientedProgramming/ObjectsAsParameters/src/com/company/Main.java
+++ ObjectOrientedProgramming/ObjectsAsParameters/src/com/company/Main.java
@@ -10,6 +10,18 @@ public class Main {
 
         System.out.println("Is ob1 == ob2? " + ob1.isEqualTo(ob2));
         System.out.println("is ob1 == ob3? " + ob1.isEqualTo(ob3));
+
+        // Creating new objects of class objects
+
+        Box box1 = new Box(15, 20, 35);
+        Box box2 = new Box(24.9);
+        Box box3 = new Box(box1);  // Passing the object box1 as a parameter to create new object box3.
+                                   // technically box1 and box3 would have different memory allocations when box3 object
+                                   // is created during run time.
+
+        System.out.println("Volume of box1 is " + box1.volume());
+        System.out.println("Volume of box2 is " + box2.volume());
+        System.out.println("Volume of box3 is " + box3.volume());
     }
 }
 
@@ -23,5 +35,31 @@ class Test {
 
     boolean isEqualTo(Test o) {
         return (this.a == o.a) && (this.b == o.b);
+    }
+}
+
+class Box{
+    private double width;
+    private double height;
+    private double depth;
+
+    public Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public Box(Box box) {
+        this.width = box.width;
+        this.height = box.height;
+        this.depth = box.depth;
+    }
+
+    public Box(double len) {
+        this.width = this.height = this.depth = len;
+    }
+
+    double volume() {
+        return this.width*this.height*this.depth;
     }
 }
