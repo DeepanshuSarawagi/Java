9e0ac745fc158d077826535c87a0d5792039f196
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-10 14:28:32 +0530

    Use recursion to print array of items

--- Recursion/src/com/company/Main.java
+++ Recursion/src/com/company/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	    factorial f = new factorial();
         System.out.println("Factorial of 5 is " + f.fact(5));
+
+        RecTest r = new RecTest(10);
+        for (int i=0; i<10; i++) {
+            r.values[i] = i;
+        }
+
+        r.printArray(10);
+
     }
 }
 
@@ -19,4 +27,22 @@ class factorial{
         return result;
     }
 
+}
+
+class RecTest {
+    int[] values;
+
+    RecTest(int i) {
+        values = new int[i];
+    }
+
+    // display array using recursion
+    void printArray(int n) {
+        if (n==0) {
+            return;
+        } else {
+            printArray(n-1);
+            System.out.println("[" + (n-1) +"] " + values[n-1]);
+        }
+    }
 }
