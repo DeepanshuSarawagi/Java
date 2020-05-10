0133500a5d49fdc76bbbc9bfe45363a9f63a0f8a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-10 14:08:11 +0530

    Learn how to get factorial of n using recursive function

--- Recursion/src/com/company/Main.java
+++ Recursion/src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    factorial f = new factorial();
+        System.out.println("Factorial of 5 is " + f.fact(5));
     }
+}
+
+class factorial{
+    int fact(int n) {
+        int result;
+        if (n==1) {
+            return 1;
+        } else {
+            result = fact(n-1) * n;
+        }
+        return result;
+    }
+
 }
