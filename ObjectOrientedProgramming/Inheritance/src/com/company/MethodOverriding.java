70e6cd9e8e0fb2e8f4cb83819120eedb1e801f04
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-11 21:39:02 +0530

    Learn how to overload a method when using Inheritance in Java

--- ObjectOrientedProgramming/Inheritance/src/com/company/MethodOverriding.java
+++ ObjectOrientedProgramming/Inheritance/src/com/company/MethodOverriding.java
@@ -6,6 +6,11 @@ public class MethodOverriding {
         objB.show();
         // You can Override methods when you implement inheritance in Java. When objB invokes the show() method, the
         // show() is class B is called. We use the super.show() to call the show() in super class 'A'.
+
+        // We are now calling the overloaded method show using the objB.
+        objB.show("This is c: ");
+        objB.show();  //if show() exists in subclass then that will be called or else show() method in superclass will
+        // be called.
     }
 }
 class A {
@@ -34,5 +39,12 @@ class B extends A{
     public void show() {
         super.show();  //this calls the show() method from the super class
         System.out.println("c is: " + c);
+    }
+
+    // Overriding occurs only when the name and the type signatures of two methods are identical.
+    // if they are not then simply two methods are overloaded. Consider the following example
+
+    public void show(String msg) {
+        System.out.println(msg + c);
     }
 }
