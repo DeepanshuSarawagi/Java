9bfecfd1e2e02b1a226901c35e22509e371b9990
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-13 21:02:05 +0530

    Create new FixedStack class implementing stack

--- ObjectOrientedProgramming/ApplyingInterfaces/src/com/company/Main.java
+++ ObjectOrientedProgramming/ApplyingInterfaces/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Create new FixedStack objects
+        FixedStack fixedStack1 = new FixedStack(10);
+
+        // push items into stack
+        for (int i=0; i<10; i++) {
+            fixedStack1.push(i);
+        }
+
+        // pop item from the stack
+        System.out.println("Pop items from the stack");
+        for (int i=0; i<10; i++) {
+            System.out.println(fixedStack1.pop());
+        }
     }
 }
