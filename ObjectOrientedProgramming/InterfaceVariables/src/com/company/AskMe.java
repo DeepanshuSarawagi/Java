61c58e72d580016eda6e6256490bf6c95f727730
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-13 22:37:48 +0530

    Variable in interfaces

---
+++ ObjectOrientedProgramming/InterfaceVariables/src/com/company/AskMe.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class AskMe implements SharedConstants{
+    static void answer(int result) {
+        switch (result) {
+            case NO:
+                System.out.println("NO");
+                break;
+            case YES:
+                System.out.println("Yes");
+                break;
+            case MAYBE:
+                System.out.println("May Be");
+                break;
+            case LATER:
+                System.out.println("Later");
+                break;
+            case SOON:
+                System.out.println("Soon");
+                break;
+            case NEVER:
+                System.out.println("Never");
+                break;
+        }
+    }
+}
--- ObjectOrientedProgramming/InterfaceVariables/src/com/company/Main.java
+++ ObjectOrientedProgramming/InterfaceVariables/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Question q = new Question();
+	    AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
     }
 }
