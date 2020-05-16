3942814188af47ce041b0939871474e5250c8bb1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-16 20:16:47 +0530

    Implement addItem and traverse method in the SearchtTree class

--- ObjectOrientedProgramming/AbstractClassChallenge/src/com/company/SearchtTree.java
+++ ObjectOrientedProgramming/AbstractClassChallenge/src/com/company/SearchtTree.java
@@ -13,7 +13,39 @@ public class SearchtTree implements NodeList{
     }
 
     @Override
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            // the list is empty, so item becomes the head of the tree
+            this.root = newItem;
+        }
+        // otherwise, start comparing from the head of the tree
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(newItem);
+            if (comparison < 0) {
+                // move right if possible
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    // if there is no node at right, add at this point
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+                } else if (comparison > 0) {
+                    // newItem is less, move left if possible
+                    if (currentItem.previous() != null) {
+                        currentItem = currentItem.previous();
+                    } else {
+                        // there is no node to the left, so add at this point
+                        currentItem.setPrevious(newItem);
+                        return true;
+                    }
+            } else {
+                // comparison is equal
+                System.out.println("Item " + newItem.getValue() + " is already present. Not adding it.");
+                return false;
+            }
+        }
         return false;
     }
 
@@ -24,6 +56,12 @@ public class SearchtTree implements NodeList{
 
     @Override
     public void traverse(ListItem root) {
-
+        //recursive function
+        if (root != null) {
+            traverse(root.previous()); {
+                System.out.println(root.getValue());
+                traverse(root.next());
+            }
+        }
     }
 }
