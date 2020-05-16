06e16135b99e8739eb25dcbe4bcfcd5af820fb3a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-16 19:56:33 +0530

    Create new Java class to search through a linkedList

---
+++ ObjectOrientedProgramming/AbstractClassChallenge/src/com/company/SearchtTree.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class SearchtTree implements NodeList{
+    private ListItem root = null;
+
+    public SearchtTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
