100b1d1a4979a4cc8796b53bfaa19c53f57c255c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-17 22:49:15 +0530

    BoundedTypes in Generics and how to use them

--- Generics/BoundedTypes/src/com/company/Main.java
+++ Generics/BoundedTypes/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Integer[] inums = {1, 2, 3, 4, 5, 6, 7};
+	    Stats<Integer> stats = new Stats<>(inums);
+        System.out.println("Average of nums is " + stats.average());
+        System.out.println();
+        Double[] dnums = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+        Stats<Double> stats1 = new Stats<>(dnums);
+        System.out.println("Average of dnums is " + stats1.average());
     }
 }
--- Generics/BoundedTypes/src/com/company/Stats.java
+++ Generics/BoundedTypes/src/com/company/Stats.java
@@ -1,4 +1,27 @@
 package com.company;
 
-public class Stats {
+//public class Stats<T> {
+public class Stats<T extends Number> {
+    T[] nums;
+
+    public Stats(T[] nums) {
+        this.nums = nums;
+    }
+
+    public double average() {
+        double sums = 0.0;
+        for (int i=0; i<nums.length; i++) {
+//            sums += nums[i].doubleValue();  // this will throw an error during compile time because compiler has no
+//                                              way to understand we are trying to create Stats objects using only
+//                                              numeric data type. Hence, we need to tell compiler to pass only numeric
+//                                              values to type T. Furthermore, we need to ensure that only numeric types
+//                                              are actually passed. To handle such situation, java provides bounded
+//                                              types. When specifying a type parameter, you can create an upper bound
+//                                              by extending T with Number superclass. <T extends superclass>
+            sums += nums[i].doubleValue();   // doubleValue is the method which is available to all the subclasses of
+                                            // type Number
+
+        }
+        return sums / nums.length;
+    }
 }
