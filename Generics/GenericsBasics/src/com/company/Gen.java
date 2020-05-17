c4e4febd0523de7321c490ffa153348f5fe7796d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-17 12:09:12 +0530

    Create a Generics class and understand its use

--- Generics/GenericsBasics/src/com/company/Gen.java
+++ Generics/GenericsBasics/src/com/company/Gen.java
@@ -1,5 +1,21 @@
 package com.company;
 
-public class Gen<T> {
+// A simple generic class of type parameter T. T will be replaced by real type when object of Gen class is created with
+// real type.
 
+public class Gen<T> {
+    T ob;               // declare an object of type T
+
+    public Gen(T ob) {  // Create a constructor passing the reference to an object of type T
+        this.ob = ob;
+    }
+
+    T getOb() {
+        return this.ob;  // return the value of object type T
+    }
+
+    void showType() {
+        System.out.println("Type of T is " +
+                this.ob.getClass().getName());
+    }
 }
--- Generics/GenericsBasics/src/com/company/Main.java
+++ Generics/GenericsBasics/src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Create a Gen object of type Integer
+        Gen<Integer> iOb = new Gen<>(88);
+
+        // show the type of data used by iOb
+        iOb.showType();
+
+        // get the value of iOb. Notice that no cast is needed.
+        System.out.println("Value is " + iOb.getOb());
+
+        // Create a Gen object for String
+        Gen<String> strObj= new Gen<>("Generics test");
+
+        // show the type of data used by strObj
+        strObj.showType();
+
+        // get value of strObj
+        System.out.println("Value of strObj is " + strObj.getOb());
     }
 }
