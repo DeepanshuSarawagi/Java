eb4825d5e6afc9c1847a35d9dacc0dbfb1fe5ffa
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-20 12:44:50 +0530

    create factorial series as per the challenge

--- CodingExercises/PackagesChallenge/src/com/series/deepanshu/Main.java
+++ CodingExercises/PackagesChallenge/src/com/series/deepanshu/Main.java
@@ -3,5 +3,7 @@ package com.series.deepanshu;
 public class Main {
     public static void main(String[] args) {
         Series.nSum(10);
+        System.out.println();
+        Series.factorial(15);
     }
 }
--- CodingExercises/PackagesChallenge/src/com/series/deepanshu/Series.java
+++ CodingExercises/PackagesChallenge/src/com/series/deepanshu/Series.java
@@ -8,4 +8,21 @@ public class Series {
             System.out.print(sum + " ");
         }
     }
+    protected static void factorial(int n) {
+        long factN = 1;
+        long factNMinus1;
+        System.out.println("First " + n + " factorials are: ");
+        if (n==0) {
+            factN = 0;
+            System.out.println("Factorial of " + n + " is " + factN);
+        } else if (n==1) {
+            System.out.println("Factorial of " + n + " is " + factN);
+        } else {
+            for (int i=2; i<=n; i++) {
+                factNMinus1 = factN;
+                factN = factNMinus1*i;
+                System.out.println("Factorial of " + i + " is " + factN);
+            }
+        }
+    }
 }
