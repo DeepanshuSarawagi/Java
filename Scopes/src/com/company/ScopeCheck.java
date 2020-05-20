fde3dde1498a8d7e2f21a4b54e5079820ec29d2a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-20 22:22:58 +0530

    Create inner class and check the scope of visibility

--- Scopes/src/com/company/ScopeCheck.java
+++ Scopes/src/com/company/ScopeCheck.java
@@ -17,4 +17,19 @@ public class ScopeCheck {
             System.out.println(i + " times two = " + i*privateVar);
         }
     }
+
+    public class InnerClass {
+        private int privateVar = 3;
+
+        public InnerClass() {
+            System.out.println("InnerClass called and privateVar value is " + privateVar);
+        }
+
+        public void timesTwo() {
+            int privateVar = 2;
+            for (int i=1; i<=10; i++) {
+                System.out.println(i + " times two = " + i*privateVar);
+            }
+        }
+    }
 }
