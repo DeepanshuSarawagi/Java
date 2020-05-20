5da23a8d4f57fe66f1f349d7c70f1c22b66e5d5f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-20 11:45:19 +0530

    build new JAR to use it later

---
+++ ObjectOrientedProgramming/Packages/PackageDemo/src/com/example/game/Monster.java
@@ -0,0 +1,55 @@
+package com.example.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable{
+    private String name;
+    private int hitPoints;
+    private int Strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        Strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return Strength;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", Strength=" + Strength +
+                '}';
+    }
+
+    @Override
+    public List<String> writeData() {
+        ArrayList<String> values = new ArrayList<>();
+        values.add(0, getName());
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.Strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.Strength = Integer.parseInt(savedValues.get(2));
+        }
+    }
+}
