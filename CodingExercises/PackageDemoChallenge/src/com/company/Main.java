5b7f1a6be7642ca303617b69fc9c5357ef347331
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-20 13:51:24 +0530

    Complete the PackageChallenge as per Udemy

--- CodingExercises/PackageDemoChallenge/src/com/company/Main.java
+++ CodingExercises/PackageDemoChallenge/src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
 
+import com.series.deepanshu.Series;
+
 public class Main {
 
     public static void main(String[] args) {
         Series.nSum(10);
+        System.out.println();
+        Series.factorial(10);
+        Series.fibonacci(20);
     }
 }
