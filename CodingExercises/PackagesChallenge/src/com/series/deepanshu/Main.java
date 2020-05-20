35da59ba4c9bf637f9df9ed23a06fe3ce81e2993
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-20 12:10:33 +0530

    create first series as per the challenge

--- CodingExercises/PackagesChallenge/src/com/series/deepanshu/Main.java
+++ CodingExercises/PackagesChallenge/src/com/series/deepanshu/Main.java
@@ -2,6 +2,6 @@ package com.series.deepanshu;
 
 public class Main {
     public static void main(String[] args) {
-
+        Series.nSum(10);
     }
 }
--- CodingExercises/PackagesChallenge/src/com/series/deepanshu/Series.java
+++ CodingExercises/PackagesChallenge/src/com/series/deepanshu/Series.java
@@ -1,5 +1,11 @@
 package com.series.deepanshu;
 
 public class Series {
-
+    protected static void nSum(int n) {
+        int sum = 0;
+        for (int i=0; i<=n; i++) {
+            sum += i;
+            System.out.print(sum + " ");
+        }
+    }
 }
