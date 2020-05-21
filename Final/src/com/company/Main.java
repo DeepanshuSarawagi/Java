f8351c45f2e52377cf953a064a3d95cc791f768f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-21 13:22:22 +0530

    learn about final keyword in Java

--- Final/src/com/company/Main.java
+++ Final/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        SomeClass one = new SomeClass("One");
+        SomeClass two = new SomeClass("Two");
+        SomeClass three = new SomeClass("Three");
 
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+//        one.InstanceNumber = 4;  // This will throw an error because its a final variable and we cannot change the
+//                                    value
     }
 }
