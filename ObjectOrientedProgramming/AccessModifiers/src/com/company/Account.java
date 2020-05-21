6e1bd440c7f826fd7fb715d5a3e5c8eb848f6d16
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-21 12:05:11 +0530

    create new Account class and define deposit and withdraw method

--- ObjectOrientedProgramming/AccessModifiers/src/com/company/Account.java
+++ ObjectOrientedProgramming/AccessModifiers/src/com/company/Account.java
@@ -15,5 +15,24 @@ public class Account {
     public int getBalance() {
         return this.balance;
     }
+    public void deposit(int amount) {
+        if (amount > 0) {
+            transactions.add(amount);
+            this.balance += amount;
+            System.out.println(amount + " deposited. Balance is now " + this.balance);
+        } else {
+            System.out.println("Cannot deposit negative sums.");
+        }
+    }
 
+    public void withdrawal(int amount) {
+        int withdrawal = -amount;
+        if (withdrawal < 0) {
+            this.transactions.add(withdrawal);
+            this.balance += withdrawal;
+            System.out.println(amount + " withdrawn. Balance is now " + this.balance);
+        } else {
+            System.out.println("Cannot withdraw negative amount.");
+        }
+    }
 }
