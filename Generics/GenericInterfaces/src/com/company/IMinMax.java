01d39c9ab2f1fa0b47936584735d9136dda0e8bd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-23 23:07:24 +0530

    Add notes on understanding of implementing Generic interface

--- Generics/GenericInterfaces/src/com/company/IMinMax.java
+++ Generics/GenericInterfaces/src/com/company/IMinMax.java
@@ -1,6 +1,12 @@
 package com.company;
 
 public interface IMinMax<T extends Comparable<T>> {
+    /**
+     * In general, a generic interface is created in the same like how a generic class is created. In this case, the
+     * type parameter is T and is extended with  ; its upper bound is Comparable. As seen earlier, Comparable is an
+     * interface defined by java.lang that specifies how objects are compared. Its type parameter specifies the type of
+     * objects being compared.
+     */
     T Min();
     T Max();
 }
--- Generics/GenericInterfaces/src/com/company/MyClass.java
+++ Generics/GenericInterfaces/src/com/company/MyClass.java
@@ -1,6 +1,15 @@
 package com.company;
 
 public class MyClass<T extends Comparable<T>> implements IMinMax<T> {
+    /**
+     * Pay special attention to the way Type parameter T is declared in MyClass and then passed to MinMax interface.
+     * Because MinMax requires a type that implements Comparable, the implementing class must specify the same bound.
+     * Further once this bound has been established, there is no need to specify it again in the implements clause. In
+     * fact, it would be wrong to do so. Following line is syntactically wrong
+     * public class MyClass<T extends Comparable<T>> implements IMinMax<T extends Comparable<T>>
+     * Once the type parameter has been established, it is simply passed to the interface without further modification
+     */
+
     T[] vals;
 
     public MyClass(T[] vals) {
