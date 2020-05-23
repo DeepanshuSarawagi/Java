a3ec7e6cc58818697688b6904779a1197f7216fb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-23 22:53:35 +0530

    Create objects of the class which implements Generic interface and test the code

--- Generics/GenericInterfaces/src/com/company/Main.java
+++ Generics/GenericInterfaces/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Integer[] nums = {6, 4, 7, 9, 2, 1, 10};
+	    MyClass<Integer> myInts = new MyClass<>(nums);
+        System.out.println("Minimum value is " + myInts.Min());
+        System.out.println("Maximum value is " + myInts.Max());
+
+        Character[] chars = {'b', 'a', 'x', 'p', 's', 'd'};
+        MyClass<Character> myChars = new MyClass<>(chars);
+        System.out.println("Minimum value is " + myChars.Min());
+        System.out.println("Maximum value is " + myChars.Max());
     }
 }
