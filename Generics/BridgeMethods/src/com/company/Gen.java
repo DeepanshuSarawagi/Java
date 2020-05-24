942d1b7a251f9a237ca64523b71ccf1cbad9b3ab
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-24 13:27:02 +0530

    Bridge methods in generics

--- Generics/BridgeMethods/src/com/company/Gen.java
+++ Generics/BridgeMethods/src/com/company/Gen.java
@@ -15,7 +15,15 @@ class Gen2 extends Gen<String> {
     /**
      * Notice that we have extended Gen2 from Gen of a specific type String.
      * Furthermore, getOb() return type is String which is overridden in the subclass.
-     */
+     * All this is perfectly fine, only trouble is expected form of getOb() will be Object getOb() { ....
+     * To handle this problem, compiler generates a bridge method with the preceding signature that calls the String
+     * version. Thus, if you examine the class file for Gen2 using javap, you will see the following methods
+     *   public com.company.Gen2(java.lang.String);
+     *   public java.lang.String getOb();
+     *   public java.lang.Object getOb();  // Bridge method
+     *
+     *   As you can see, bridge method is included when the java class is compiled.
+     **/
     public Gen2(String ob) {
         super(ob);
     }
