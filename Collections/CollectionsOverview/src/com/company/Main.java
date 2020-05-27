117274d61fcef709226fc3c66108648dba3168d8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-27 20:46:50 +0530

    Create new ArrayList and copy the Theatre seats in it and print it.

--- Collections/CollectionsOverview/src/com/company/Main.java
+++ Collections/CollectionsOverview/src/com/company/Main.java
@@ -1,21 +1,35 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("SPI Cinemas", 8, 12);
-        theatre.getSeats();
-        if (theatre.reserveSeat("H10")) {
-            System.out.println("Please pay.");
-        } else {
-            System.out.println("Seat is already taken.");
-        }
+//        theatre.getSeats();
+//        if (theatre.reserveSeat("H10")) {
+//            System.out.println("Please pay.");
+//        } else {
+//            System.out.println("Seat is already taken.");
+//        }
+//
+//        if (theatre.reserveSeat("A10")) {
+//            System.out.println("Please pay.");
+//        } else {
+//            System.out.println("Seat is already taken.");
+//        }
 
-        if (theatre.reserveSeat("A10")) {
-            System.out.println("Please pay.");
-        } else {
-            System.out.println("Seat is already taken.");
-        }
+        // Create a new list and copy the theatre seats in the ArrayList
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
+    }
 
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("======================================");
     }
 }
