03f4bcea5134baee0ae20910e46fc0fb07cc0e2e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-28 11:04:49 +0530

    Create new project to learn about Maps

---
+++ Collections/Maps/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * Basics of Map Collection in Java
+         */
+
+        Map<String, String> languages = new HashMap<>();
+    }
+}
