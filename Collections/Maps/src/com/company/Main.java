4ecb0f47ac0f4cbb985e36692ba317156dfd54fd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-28 11:10:44 +0530

    Basics of Map

--- Collections/Maps/src/com/company/Main.java
+++ Collections/Maps/src/com/company/Main.java
@@ -11,5 +11,13 @@ public class Main {
          */
 
         Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "A compiled, high-level, object-oriented, platform independent language");
+        languages.put("Python", "A interpreter high level, object-oriented language with dynamic semantics");
+        languages.put("BASIC", "Beginners All purposes Symbolic Instruction Code");
+        languages.put("Algol", "An algorithmic language");
+
+        System.out.println(languages.get("Java"));
+        languages.put("Java", "This course is about Java");
+        System.out.println(languages.get("Java"));  // This overwrites the value of key Java
     }
 }
