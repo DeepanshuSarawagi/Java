6e370905b6d1260de8cf356c866a6a5d0f6c02b7
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-03 15:40:44 +0530

    Create new StockList class which will have a Map of StockItem and create constructor

---
+++ Collections/SortedCollections/src/com/company/StockList.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new HashMap<>();
+    }
+}
