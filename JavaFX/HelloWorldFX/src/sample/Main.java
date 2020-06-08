b440cb6211b25223b2c692ae70de519eb2158f20
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-08 12:06:27 +0530

    Change the font size and font weight

--- JavaFX/HelloWorldFX/src/sample/Main.java
+++ JavaFX/HelloWorldFX/src/sample/Main.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -22,9 +24,10 @@ public class Main extends Application {
 
         Label greeting = new Label("Welcome to JavaFX!");  // create a new label using Label controller
         greeting.setTextFill(Color.GREEN);  // Set the text of the label to Green
+        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
         root.getChildren().add(greeting);   // Add the greeting to the GridPane
         primaryStage.setTitle("Hello JavaFX!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
     }
 
