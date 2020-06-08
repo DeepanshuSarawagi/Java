451d300cbf572fb6476df86d568eab16a87f15f9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-08 12:02:43 +0530

    Create new Label and set the text color to Green

--- JavaFX/HelloWorldFX/src/sample/Main.java
+++ JavaFX/HelloWorldFX/src/sample/Main.java
@@ -2,16 +2,28 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX!");  // create a new label using Label controller
+        greeting.setTextFill(Color.GREEN);  // Set the text of the label to Green
+        root.getChildren().add(greeting);   // Add the greeting to the GridPane
+        primaryStage.setTitle("Hello JavaFX!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
