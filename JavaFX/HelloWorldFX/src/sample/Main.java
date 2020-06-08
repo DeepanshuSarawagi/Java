12463323fe6ee87092fe4b94e4b4064e759ea97b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-08 12:14:15 +0530

    Learn how to do the previous commit changes using fxml

--- JavaFX/HelloWorldFX/src/sample/Main.java
+++ JavaFX/HelloWorldFX/src/sample/Main.java
@@ -16,16 +16,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
-
-        Label greeting = new Label("Welcome to JavaFX!");  // create a new label using Label controller
-        greeting.setTextFill(Color.GREEN);  // Set the text of the label to Green
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-        root.getChildren().add(greeting);   // Add the greeting to the GridPane
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX!");  // create a new label using Label controller
+//        greeting.setTextFill(Color.GREEN);  // Set the text of the label to Green
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//        root.getChildren().add(greeting);   // Add the greeting to the GridPane
         primaryStage.setTitle("Hello JavaFX!");
         primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
