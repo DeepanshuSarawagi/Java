300c8863933068217f7aacf5487cc984cb721e78
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-13 12:58:59 +0530

    How to associate a TextField in Controller.java

--- JavaFX/Events/src/sample/Controller.java
+++ JavaFX/Events/src/sample/Controller.java
@@ -1,7 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML                               // Annotate nameField as FXML to associate it with TextField in FXML
+    private TextField nameField;
     public void onButtonClicked() {
-        System.out.println("Hello!!");
+        System.out.println("Hello!! " + nameField.getText());
     }
 }
