66a0839c4f7a4c8038d765079e4dedd39893c8ff
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-13 13:13:25 +0530

    Learn how to associate multiple button actions with single method

--- JavaFX/Events/src/sample/Controller.java
+++ JavaFX/Events/src/sample/Controller.java
@@ -1,12 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML                               // Annotate nameField as FXML to associate it with TextField in FXML
     private TextField nameField;
-    public void onButtonClicked() {
-        System.out.println("Hello!! " + nameField.getText());
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
     }
 }
