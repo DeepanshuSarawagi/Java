407b8b936ca17814aee3e3338b1ee405f14c3dbe
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-14 19:07:12 +0530

    Create new Java class for the TodoItems

---
+++ JavaFX/TodoList/src/com/deepanshu/todolist/datamodel/TodoItems.java
@@ -0,0 +1,39 @@
+package com.deepanshu.todolist.datamodel;
+
+import java.time.LocalDate;
+
+public class TodoItems {
+    private String shortDescription;
+    private String details;
+    private LocalDate deadLine;
+
+    public TodoItems(String shortDescription, String details, LocalDate deadLine) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadLine = deadLine;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+}
