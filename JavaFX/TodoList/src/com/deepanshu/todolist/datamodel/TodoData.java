ee02346b5de315c6ee5c06f276d00b12dd2e9145
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-15 10:50:27 +0530

    Create new class to store the TodoItems and its details in a text file

---
+++ JavaFX/TodoList/src/com/deepanshu/todolist/datamodel/TodoData.java
@@ -0,0 +1,20 @@
+package com.deepanshu.todolist.datamodel;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class TodoData {
+    private static TodoData instance = new TodoData();
+    private static String fileName = "TodoListItems.txt";
+
+    private List<TodoItem> todoItems;
+    private DateTimeFormatter formatter;
+
+    public static TodoData getInstance() {
+        return instance;
+    }
+
+    private TodoData() {
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+}
