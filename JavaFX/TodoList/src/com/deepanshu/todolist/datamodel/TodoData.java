918d92f2975d4de7239dce2dcc0eb2c328b5e3d1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-16 21:34:45 +0530

    Define a method to load the TodoItems data from a text file

--- JavaFX/TodoList/src/com/deepanshu/todolist/datamodel/TodoData.java
+++ JavaFX/TodoList/src/com/deepanshu/todolist/datamodel/TodoData.java
@@ -1,5 +1,13 @@
 package com.deepanshu.todolist.datamodel;
 
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -24,5 +32,30 @@ public class TodoData {
 
     public void setTodoItems(List<TodoItem> todoItems) {
         this.todoItems = todoItems;
+    }
+
+    public void loadTodoItems() throws IOException {
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(fileName);
+        BufferedReader br = Files.newBufferedReader(path);
+        String input;
+
+        try {
+            while ((input = br.readLine()) != null) {
+                String[] itemPieces = input.split("\t");
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateSting = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateSting, formatter);
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItems.add(todoItem);
+            }
+        } finally {
+            if (br!=null) {
+                br.close();
+            }
+        }
+
     }
 }
