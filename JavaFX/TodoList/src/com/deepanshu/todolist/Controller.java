c8a528f31240119ef0d9adf60a07ebe78f9ebd88
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-18 22:50:00 +0530

    Add functionality to select the newly added TodoItem

--- JavaFX/TodoList/src/com/deepanshu/todolist/Controller.java
+++ JavaFX/TodoList/src/com/deepanshu/todolist/Controller.java
@@ -88,8 +88,9 @@ public class Controller {
         Optional<ButtonType> result = dialog.showAndWait();
         if (result.isPresent() && result.get() == ButtonType.OK) {
             DialogController controller = fxmlLoader.getController();
-            controller.processResults();
+            TodoItem item = controller.processResults();
             todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+            todoListView.getSelectionModel().select(item);
             System.out.println("Okay button pressed");
         } else {
             System.out.println("Cancel button pressed");
