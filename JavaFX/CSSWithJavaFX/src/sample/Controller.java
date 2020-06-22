b1313ffffb8443d3d8acd6558483f8b32b29fabd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-22 22:04:30 +0530

    Learn how to zoom in a label using FXMl and controller code

--- JavaFX/CSSWithJavaFX/src/sample/Controller.java
+++ JavaFX/CSSWithJavaFX/src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        label.setScaleX(1.5);
+        label.setScaleY(1.5);
+    }
+
 }
