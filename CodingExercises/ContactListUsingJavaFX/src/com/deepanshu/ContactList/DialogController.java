8ecce7f67963797ffd83cb402c3db1572063b69c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-06-29 21:03:28 +0530

    Associate NewContactDialog.fxml with DialogController

--- CodingExercises/ContactListUsingJavaFX/src/com/deepanshu/ContactList/DialogController.java
+++ CodingExercises/ContactListUsingJavaFX/src/com/deepanshu/ContactList/DialogController.java
@@ -1,5 +1,30 @@
 package com.deepanshu.ContactList;
 
+import com.deepanshu.ContactList.dataModel.Contact;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class DialogController {
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField email;
+
+    public Contact createNewContact() {
+        String contactFirstName = firstName.getText().trim();
+        String contactLastName = lastName.getText().trim();
+        String contactPhoneNumber = phoneNumber.getText().trim();
+        String contactEmail = email.getText().trim();
+
+        return new Contact(contactFirstName, contactLastName, contactPhoneNumber, contactEmail);
+    }
 
 }
