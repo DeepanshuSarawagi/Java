ad0fd0d7a7c2f8bff5322254a9d1ad4c4b2099a9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-01 21:22:05 +0530

    Add the TableView and TableColumns in the mainWindow.fxml

--- JavaFX/MyContacts/src/com/deepanshu/contacts/Controller.java
+++ JavaFX/MyContacts/src/com/deepanshu/contacts/Controller.java
@@ -1,4 +1,11 @@
 package com.deepanshu.contacts;
 
+import com.deepanshu.contacts.dataModel.Contact;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class Controller {
+
+    @FXML
+    private TableView<Contact> contactsTable;
 }
