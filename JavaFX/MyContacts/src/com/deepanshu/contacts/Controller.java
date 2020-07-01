fbef3cdbca4576da4d09c809a1bc31c938ed490e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-01 21:40:14 +0530

    Create new Dialog pane

--- JavaFX/MyContacts/src/com/deepanshu/contacts/Controller.java
+++ JavaFX/MyContacts/src/com/deepanshu/contacts/Controller.java
@@ -2,8 +2,14 @@ package com.deepanshu.contacts;
 
 import com.deepanshu.contacts.dataModel.Contact;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class Controller {
 
@@ -15,6 +21,25 @@ public class Controller {
 
     @FXML
     public void showAddContactDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainPanel.getScene().getWindow());
+        dialog.setTitle("Add new Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactDialog.fxml"));
 
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+
+        }
     }
 }
