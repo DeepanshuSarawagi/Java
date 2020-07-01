86ecc9371b98745a3ecdf15906aae350f34a7d02
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-01 21:07:44 +0530

    create getters and setters

--- JavaFX/MyContacts/src/com/deepanshu/contacts/dataModel/Contact.java
+++ JavaFX/MyContacts/src/com/deepanshu/contacts/dataModel/Contact.java
@@ -18,5 +18,52 @@ public class Contact {
     public Contact() {
     }
 
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber.get();
+    }
+
+    public SimpleStringProperty phoneNumberProperty() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber.set(phoneNumber);
+    }
+
+    public String getNotes() {
+        return notes.get();
+    }
+
+    public SimpleStringProperty notesProperty() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes.set(notes);
+    }
 
 }
