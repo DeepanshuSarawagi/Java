dbe69fe48119ffe8b699ca2b653d6bcf9b29d6fb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-01 21:34:41 +0530

    Create new ContactDialog.java and contactDialog.fxml for creating new contacts

---
+++ JavaFX/MyContacts/src/com/deepanshu/contacts/ContactDialog.java
@@ -0,0 +1,19 @@
+package com.deepanshu.contacts;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ContactDialog {
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField notesField;
+
+}
--- JavaFX/MyContacts/src/com/deepanshu/contacts/Controller.java
+++ JavaFX/MyContacts/src/com/deepanshu/contacts/Controller.java
@@ -3,9 +3,18 @@ package com.deepanshu.contacts;
 import com.deepanshu.contacts.dataModel.Contact;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 public class Controller {
 
     @FXML
     private TableView<Contact> contactsTable;
+
+    @FXML
+    private BorderPane mainPanel;
+
+    @FXML
+    public void showAddContactDialog() {
+
+    }
 }
