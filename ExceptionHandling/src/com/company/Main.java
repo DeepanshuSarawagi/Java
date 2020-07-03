718ca97cbe3305d891927f0918a126b60dcc71cc
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-03 21:39:08 +0530

    Learn about two ways of exception handling: LBYL, EAFP

--- ExceptionHandling/src/com/company/Main.java
+++ ExceptionHandling/src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x = 98;
+        int y = 0;  // this is deliberately done to raise error
+
+        System.out.println(divideEAFP(x, y));
+        System.out.println(divideLBYL(x, y));
+    }
+
+    /*
+    We are going to learn Exception Handling using two methods. One is Look Before You Leave (LBYL) and other one is
+    Easy to Ask for Forgiveness and Permission.
+     */
+    private static int divideLBYL(int x, int y) {
+        if (y != 0) {
+            return x / y;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int divideEAFP(int x, int y) {
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
     }
 }
