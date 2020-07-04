5216a239027f7506fe47be17622469aedea57cb1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-04 16:15:38 +0530

    Learn how to catch InputMismatchException using EAFP method

--- ExceptionHandling/src/com/company/callStack.java
+++ ExceptionHandling/src/com/company/callStack.java
@@ -1,11 +1,17 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class callStack {
     public static void main(String[] args) {
         int x = getInt();
         int y = getInt();
+
+        System.out.println(divide(x, y));
+
+        x = getIntEAFP();
+        y = getIntEAFP();
 
         System.out.println(divide(x, y));
     }
@@ -20,6 +26,20 @@ public class callStack {
             hasNextInt = scanner.hasNextInt();
         }
         return scanner.nextInt();
+    }
+
+    private static int getIntEAFP() {
+        System.out.println("Please enter an integer of your choice: ");
+        Scanner scanner = new Scanner(System.in) ;
+
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("That was an invalid input. Please try again: ");
+            }
+        }
     }
 
     private static int divide(int x, int y) {
