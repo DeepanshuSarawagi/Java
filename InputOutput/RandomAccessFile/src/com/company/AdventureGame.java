43ddc4a318134b41c1364eb7883f1c47c9894936
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-08 12:31:38 +0530

    Modify the AdventureGame to get the Locations using getLocation

--- InputOutput/RandomAccessFile/src/com/company/AdventureGame.java
+++ InputOutput/RandomAccessFile/src/com/company/AdventureGame.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class AdventureGame {
     private static Locations locations = new Locations();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         // Improvise the game by getting the input from user as a phrase and map those phrase with keys of exits
@@ -19,13 +20,15 @@ public class AdventureGame {
         vocabulary.put("DOWN", "D");
         vocabulary.put("UP", "U");
 
-        int loc = 1;
+
+        Location currentLocation = locations.getLocation(64);
+
         while (true) {
-            System.out.println(locations.get(loc).getDescription());
-            if (loc == 0) {
+            System.out.println(currentLocation.getDescription());
+            if (currentLocation.getLocationID() == 0) {
                 break;
             }
-            Map<String, Integer> exits = locations.get(loc).getExits();
+            Map<String, Integer> exits = currentLocation.getExits();
             System.out.print("Available exits are: ");
             for (String exit: exits.keySet()) {
                 System.out.print(exit + " ");
@@ -41,10 +44,12 @@ public class AdventureGame {
                 }
             }
             if (exits.containsKey(direction)) {
-                loc = exits.get(direction);
+                currentLocation = locations.getLocation(currentLocation.getExits().get(direction));
             } else {
                 System.out.println("You cannot go in that direction");
             }
         }
+
+        locations.close();
     }
 }
