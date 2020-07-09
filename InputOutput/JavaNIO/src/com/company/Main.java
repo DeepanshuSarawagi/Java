9ac984a01f0c1c96ad1315907504380881012a3f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-09 17:56:27 +0530

    Reading Lines using JavaNIO

--- InputOutput/JavaNIO/src/com/company/Main.java
+++ InputOutput/JavaNIO/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Main {
 
@@ -11,10 +15,16 @@ public class Main {
         // FileOutputStream and RandomAccessFile
 
         try {
-            FileInputStream file = new FileInputStream("data.txt");
-            FileChannel channel = file.getChannel();  // Getting the channel of the file using FileInputStream instance
-                                                      // and method getChannel
+//            FileInputStream file = new FileInputStream("data.txt");
+//            FileChannel channel = file.getChannel();  // Getting the channel of the file using FileInputStream instance
+//                                                      // and method getChannel. We only need one channel for reading and
+//                                                      // writing
 
+            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+            List<String> lines = Files.readAllLines(dataPath);
+            for (String line : lines) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
             System.out.println("IOException " + e.getMessage());
         }
