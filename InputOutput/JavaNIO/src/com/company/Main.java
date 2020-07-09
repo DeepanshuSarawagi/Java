3aa310eb50131aa943176afca57a2c3fded3cd9b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-09 17:49:53 +0530

    Get the channel of the file using FileInputStream instance

--- InputOutput/JavaNIO/src/com/company/Main.java
+++ InputOutput/JavaNIO/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
 public class Main {
 
     public static void main(String[] args) {
+        // we will be creating channel to read and write file. We can create channel using FileInputStream,
+        // FileOutputStream and RandomAccessFile
+
+        try {
+            FileInputStream file = new FileInputStream("data.txt");
+            FileChannel channel = file.getChannel();  // Getting the channel of the file using FileInputStream instance
+                                                      // and method getChannel
+
+        } catch (IOException e) {
+            System.out.println("IOException " + e.getMessage());
+        }
 
     }
 }
