4cd02a3ef0e84bd6cd8c6654a0fd427c6a3f3c78
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-09 18:09:13 +0530

    Learn the theory behind the Files.write() method

--- InputOutput/JavaNIO/src/com/company/Main.java
+++ InputOutput/JavaNIO/src/com/company/Main.java
@@ -24,6 +24,17 @@ public class Main {
 
             Path dataPath = FileSystems.getDefault().getPath("data.txt");
             Files.write(dataPath, "\nLine 4".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+
+            /**
+             * The first parameter in the Files.write method is to specify the path of the file.
+             * Second parameter is to specify the data which needs to be written and then convert the data to getBytes
+             * since this method writes the data in Bytes. Hence, we are converting the String to Bytes using getBytes
+             * method and specify the Charset to UTF-8.
+             * Third paramter is specify the openOption. If we do not specify this then the file will be truncated and
+             * will be replaced with new content. Hence we need to specify the StandardOpenOption.APPEND constant to
+             * write the data along with the existing data in the file.
+             */
+
             List<String> lines = Files.readAllLines(dataPath);
             for (String line : lines) {
                 System.out.println(line);
