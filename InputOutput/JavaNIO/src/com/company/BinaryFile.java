6dade2f226057de1ad20f0e47c2dc570327bcbed
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-11 16:45:38 +0530

    Learn about writing integers in the dat file using channel and to reset the buffer position using flip method

--- InputOutput/JavaNIO/src/com/company/BinaryFile.java
+++ InputOutput/JavaNIO/src/com/company/BinaryFile.java
@@ -11,14 +11,30 @@ public class BinaryFile {
          * In this lesson we are going to learn about writing Binary files using Java NIO.
          */
 
-        try {
-            FileOutputStream binFile = new FileOutputStream("data.dat");
-            FileChannel binChannel = binFile.getChannel();
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()) {
             byte[] outputByte = "Hello World!".getBytes();   // We are calling this create the byte array and convert the
                                                             // string to byte using getByte() method.
             ByteBuffer buffer = ByteBuffer.wrap(outputByte);
             int bytes = binChannel.write(buffer);
             System.out.println("Number of bytes written " + bytes);
+
+            // Now we are going to write the integer to the dat file
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            bytes = binChannel.write(intBuffer);
+            System.out.println("Number of bytes written are " + bytes);
+
+            // After printing the above line, you will notice that no. of bytes written is zero. That's because, when we
+            // created the buffer, we created it at position zero. Hence, we put the integer in the buffer using the
+            // putInt() method, it changed the buffer position. And when we wrote the buffer in the channel, it will
+            // start reading the bytes from the buffer's position. So we have to explicitly reset the position of the
+            // buffer to start reading it from the beginning. To reset the buffer, we need to call the buffer.flip()
+            // method which resets the buffer position to zero and discards any marks left behind. We didn't have to
+            // reset the position of the buffer when we used the wrap method, that's because it automatically resets the
+            // buffer's position to zero.
 
             // Next we call the static method ByteBuffer.wrap() method to wrap the byte array into Buffer. Modifications
             // to the buffer will change the array and modifications to the array will change the buffer. When we create
