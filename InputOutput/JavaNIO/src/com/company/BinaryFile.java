1ba3a229539467fc2e49d21e027ba3f1b41bcd61
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-11 16:53:26 +0530

    Learn more about writing integers in the buffer and importance of flip method

--- InputOutput/JavaNIO/src/com/company/BinaryFile.java
+++ InputOutput/JavaNIO/src/com/company/BinaryFile.java
@@ -15,6 +15,13 @@ public class BinaryFile {
             FileChannel binChannel = binFile.getChannel()) {
             byte[] outputByte = "Hello World!".getBytes();   // We are calling this create the byte array and convert the
                                                             // string to byte using getByte() method.
+
+            // Next we call the static method ByteBuffer.wrap() method to wrap the byte array into Buffer. Modifications
+            // to the buffer will change the array and modifications to the array will change the buffer. When we create
+            // the, it sets the position to 0. When we do the read/write operation in the buffer, its start position
+            // will be at zero. Also, it sets the buffer capacity based on the byte array length and the buffer mark will
+            // be undefined.
+
             ByteBuffer buffer = ByteBuffer.wrap(outputByte);
             int bytes = binChannel.write(buffer);
             System.out.println("Number of bytes written " + bytes);
@@ -36,11 +43,19 @@ public class BinaryFile {
             // reset the position of the buffer when we used the wrap method, that's because it automatically resets the
             // buffer's position to zero.
 
-            // Next we call the static method ByteBuffer.wrap() method to wrap the byte array into Buffer. Modifications
-            // to the buffer will change the array and modifications to the array will change the buffer. When we create
-            // the, it sets the position to 0. When we do the read/write operation in the buffer, its start position
-            // will be at zero. Also, it sets the buffer capacity based on the byte array length and the buffer mark will
-            // be undefined.
+            // write a large negative integer to the channel
+
+            // Now if we run the main method, the program will crash with java.nio.BufferOverflowException. Reason
+            // behind this is, we allocated the max Integer Bytes which 4 (One Integer has 4 Bytes). So, the buffer
+            // position will be at 4. And if we do not reset the buffer to put a new Integer, it will throw
+            // ByteOverflowException. Hence, we need to reset the buffer position to zero and then again reset it to
+            // zero before writing the buffer to the channel.
+
+            intBuffer.flip();
+            intBuffer.putInt(-9813449);
+            intBuffer.flip();
+            bytes = binChannel.write(intBuffer);
+            System.out.println("Number of bytes written are " + bytes);
 
         } catch (IOException e) {
             System.out.println("IoException " + e.getMessage());
