1086092a7f7c9575b5b7721f5832e2c7a05495fa
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-13 21:38:39 +0530

    Use Paths nio package to read the contents of a file

--- InputOutput/Paths/src/com/company/Main.java
+++ InputOutput/Paths/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        PrintFile(path);
+    }
 
+    private static void PrintFile(Path path) {
+        try (BufferedReader fileReader = Files.newBufferedReader(path)){
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
