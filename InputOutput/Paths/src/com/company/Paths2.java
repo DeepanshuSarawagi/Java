d48bde221e2e9ca9d46b270bd7d3278d9cb18e01
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-14 19:57:24 +0530

    Learn how to make a copy of a file using Files.copy() method

---
+++ InputOutput/Paths/src/com/company/Paths2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Paths2 {
+    public static void main(String[] args) {
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples", "filecopy.txt");
+            Files.copy(sourceFile, copyFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
