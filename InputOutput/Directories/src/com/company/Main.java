86205596bfe723584ea98e28df4dc451196e886c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-15 17:41:19 +0530

    How to print the directory contents

--- InputOutput/Directories/src/com/company/Main.java
+++ InputOutput/Directories/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
+        try(DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            e.printStackTrace();
+        }
     }
 }
