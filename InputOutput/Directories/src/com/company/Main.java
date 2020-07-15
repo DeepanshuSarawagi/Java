14b5fcc5adaccfa5164603b52bc21784450a5f36
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-15 17:57:56 +0530

    Create your own filter

--- InputOutput/Directories/src/com/company/Main.java
+++ InputOutput/Directories/src/com/company/Main.java
@@ -7,11 +7,34 @@ public class Main {
 
     public static void main(String[] args) {
         Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
-        try(DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+
+        try(DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.txt")) {
+            // add the second parameter in the method newDirectoryStream(glob) to file any files with a specific
+            // extension.
             for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
         } catch (IOException | DirectoryIteratorException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
+        // We can also create our own filter using the class DirectoryStream.Filter<T>
+
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path entry) throws IOException {
+                return Files.isRegularFile(entry);
+            }
+        };
+
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)){
+            for (Path path : contents) {
+                System.out.println(path.getFileName());
+            }
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
