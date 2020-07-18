df9e6d8ed15b82b9dd4324b55b033881b4cead2f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-18 22:31:47 +0530

    Learn how to copy files in FileTree

---
+++ InputOutput/Directories/src/com/company/CopyFiles.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class CopyFiles extends SimpleFileVisitor<Path> {
+    private Path sourceRoot;
+    private Path targetRoot;
+
+    public CopyFiles(Path sourceRoot, Path targetRoot) {
+        this.sourceRoot = sourceRoot;
+        this.targetRoot = targetRoot;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error accessing file " + file.toAbsolutePath() + " " + exc.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return super.preVisitDirectory(dir, attrs);
+    }
+}
