11ee37b4bc6ce72453bd9f486b4c914503d0b84b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-18 21:54:41 +0530

    Implement the SimpleFileVisitor interface and override methods

--- InputOutput/Directories/src/com/company/FileWalkTree.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public class FileWalkTree {
-    // This class is created to learn about FileVisitor class
-}
---
+++ InputOutput/Directories/src/com/company/PrintNames.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class PrintNames extends SimpleFileVisitor<Path> {
+    // This class is created to learn about FileVisitor class
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println(dir.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+}
