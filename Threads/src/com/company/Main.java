b3b043a17d259cace2b20927de015e67c29fa391
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-20 10:41:13 +0530

    Execute the thread from AnotherThread class

--- Threads/src/com/company/Main.java
+++ Threads/src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from the main thread!");
+
+        // Create instance of AnotherThread class and invoke its run method
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();  // invoke the run method by calling the start() method of Thread instance
+        System.out.println("Hello again from main thread");
     }
 }
