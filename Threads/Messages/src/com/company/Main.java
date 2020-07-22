f4c5624d79b45804cd3b6d245b8577d4c8ae69fd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-22 11:59:25 +0530

    Create a Writer class to write messages

--- Threads/Messages/src/com/company/Main.java
+++ Threads/Messages/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -25,5 +27,31 @@ class Message{
         }
         empty = false;
         this.message = message;
+    }
+}
+
+class Writer implements Runnable{
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String[] messages = {"Humpty Dumpty sat on the wall",
+                             "Humpty Dumpty had a great fall",
+                             "All the king`s horses and king`s men",
+                             "Couldn't put Humpty together again"};
+        Random random = new Random();
+
+        for (int i=0; i<messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        message.write("Finished");
     }
 }
