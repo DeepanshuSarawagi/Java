91761f29cbff6760f1d385f3d1e2ee143daf5ba8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-22 11:47:28 +0530

    Create new project to learn more about Synchronization in Java Threads

---
+++ Threads/Messages/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Message{
+    private String message;
+    private boolean empty = true;
+
+    public synchronized String read() {
+
+    }
+}
