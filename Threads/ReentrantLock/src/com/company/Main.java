11506dd79b00005ce8876664f12b1823cc0fc111
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-26 16:43:38 +0530

    Create new MyProducer class to learn more about ReentrantLock

--- Threads/ReentrantLock/src/com/company/Main.java
+++ Threads/ReentrantLock/src/com/company/Main.java
@@ -1,8 +1,53 @@
 package com.company;
 
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+
+class MyProducer implements Runnable {
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
+
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String[] nums = {"1", "2", "3", "4", "5"};
+
+        try {
+            for (String num : nums) {
+                System.out.println(color + "Adding.. " + num);
+                bufferLock.lock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
+                Thread.sleep(random.nextInt(1000));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(color + "Adding EOF and exiting..");
+        bufferLock.lock();
+        try {
+            buffer.add("EOF");
+        } finally {
+            bufferLock.unlock();
+        }
     }
 }
 
