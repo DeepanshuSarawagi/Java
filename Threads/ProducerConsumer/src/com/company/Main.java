b25f7f34c316a03e5a895f16a4ed585ff29c77ed
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-26 12:51:03 +0530

    Learn the important of synchronizing the methods since ArrayList is not thread safe

--- Threads/ProducerConsumer/src/com/company/Main.java
+++ Threads/ProducerConsumer/src/com/company/Main.java
@@ -1,11 +1,24 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 import static com.company.Main.EOF;
+
+/**
+ * Why do we need to synchronize methods in class MyProducer and MyConsumer? Again, the reason is very simple.
+ * We have created two threads in class Main, one for Producer and two for consumers. When Producer puts a
+ * message/string in the ArrayList, the thread suspends, then Consumer1 thread runs and checks if List is empty and
+ * returns false and gets suspended. Following that, Consumer 2 thread runs and checks if list is empty and it returns
+ * false, following that it removes the element from the list. When consumer1 thread runs again, it resumes to remove
+ * the element from the list and the result will be ArrayIndexOutOfBoundsException because the element has already been
+ * removed by thread Consumer2.
+ *
+ * As we learned in the lecture, in the Collections framework, ArrayList is not Thread safe or Thread synchronized. And
+ * since we are sharing the same ArrayList among three threads, we would run into many problems like this. To avoid this
+ * problem, we need to synchronize the methods/statement in both MyProducer and MyConsumer classes.
+ */
 
 public class Main {
 
