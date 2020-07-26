67103e84656ed7d3d2658eda829e20f9a079e5af
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-26 17:10:38 +0530

    Count the no. of locks it had to try to actually get one and execute code

--- Threads/ReentrantLock/src/com/company/Main.java
+++ Threads/ReentrantLock/src/com/company/Main.java
@@ -77,20 +77,25 @@ class MyConsumer implements Runnable {
 
     @Override
     public void run() {
+        int counter = 0;
         while (true) {
-            bufferLock.lock();
-            try {
-                if (buffer.isEmpty()) {
-                    continue;
+            if (bufferLock.tryLock()) {
+                try {
+                    if (buffer.isEmpty()) {
+                        continue;
+                    }
+                    System.out.println("The counter is " + counter);
+                    if (buffer.get(0).equals(EOF)) {
+                        System.out.println("Reached EOF and exiting");
+                        break;
+                    } else {
+                        System.out.println(color + "removing " + buffer.remove(0));
+                    }
+                } finally {
+                    bufferLock.unlock();
                 }
-                if (buffer.get(0).equals(EOF)) {
-                    System.out.println("Reached EOF and exiting");
-                    break;
-                } else {
-                    System.out.println(color + "removing " + buffer.remove(0));
-                }
-            } finally {
-                bufferLock.unlock();
+            } else {
+                counter++;
             }
         }
     }
