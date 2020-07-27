edfcac9d19a0e49dd5bef8fce7aa4f4e45017bd6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-27 10:57:58 +0530

    Use the ExecutorService to run the threads

--- Threads/ExecutorService/src/com/company/Main.java
+++ Threads/ExecutorService/src/com/company/Main.java
@@ -3,11 +3,23 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static com.company.Main.EOF;
 
 public class Main {
+
+    /**
+     * In this lesson, we are going to learn about ExecutorService implementation. Using this interface, we need not
+     * worry about Thread Scheduling, execution, termination. It allows us to focus on executing the code than managing
+     * the threads along with code. It uses the concept of Thread Pools in Java. In typical scenario, developer tests
+     * the application multiple times to check how many threads are needed to run the application without any hassle.
+     *
+     * Instead of starting the threads manually, we can just call the execute() method of this interface.
+     *
+     */
 
     public static final String EOF = "EOF";
 
@@ -15,13 +27,23 @@ public class Main {
 
         List<String> buffer = new ArrayList<>();
         ReentrantLock bufferLock = new ReentrantLock(true);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
         MyProducer producer = new MyProducer(ThreadColor.ANSI_BLUE, buffer, bufferLock);
         MyConsumer consumer1 = new MyConsumer(ThreadColor.ANSI_CYAN, buffer, bufferLock);
         MyConsumer consumer2 = new MyConsumer(ThreadColor.ANSI_PURPLE, buffer, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+//        new Thread(producer).start();
+//        new Thread(consumer1).start();
+//        new Thread(consumer2).start();
+
+        // We are going to use the ExecutorService to run the execution of threads.
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
     }
 }
 
