a578466caafb4dd643328e893cf9202d4c539d04
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-29 09:35:24 +0530

    Understand the definition of DeadLocks

--- Threads/DeadLock/src/com/company/Main.java
+++ Threads/DeadLock/src/com/company/Main.java
@@ -2,6 +2,14 @@ package com.company;
 
 public class Main {
 
+    /**
+     * A Deadlock occurs when two or more threads are blocked on Locks. Every blocked thread is holding the lock
+     * which another blocked thread wants. A common scenario is that Thread-1 is holding the lock1 and waiting to
+     * acquire lock2 and Thread-2 is holding the lock2 and waiting to acquire lock1. None of the locks are waiting and
+     * and they continue to wait until the lock is released by either of the thread and finally fails to run.
+     *
+     */
+
     public static Object lock1 = new Object();
     public static Object lock2 = new Object();
 
