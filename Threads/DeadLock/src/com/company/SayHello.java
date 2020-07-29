91692c45e947abd97d011eb79fdf1b3a9b4e7416
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-29 10:47:22 +0530

    Avoid DeadLocks by acquiring locks on the objects

--- Threads/DeadLock/src/com/company/SayHello.java
+++ Threads/DeadLock/src/com/company/SayHello.java
@@ -1,32 +1,63 @@
 package com.company;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SayHello {
     public static void main(String[] args) {
-        PolitePerson jane = new PolitePerson("Jane");
-        PolitePerson john = new PolitePerson("John");
+        ReentrantLock bufferLock = new ReentrantLock();
+        PolitePerson jane = new PolitePerson("Jane", bufferLock);
+        PolitePerson john = new PolitePerson("John", bufferLock);
 
-        jane.SayHello(john);
-        john.SayHello(jane);
+//        jane.SayHello(john);
+//        john.SayHello(jane);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                jane.SayHello(john);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                john.SayHello(jane);
+            }
+        }).start();
     }
 
     private static class PolitePerson{
         private final String name;
+        private ReentrantLock bufferLock;
 
-        public PolitePerson(String name) {
+        public PolitePerson(String name, ReentrantLock bufferLock) {
             this.name = name;
+            this.bufferLock = bufferLock;
         }
 
         public String getName() {
             return name;
         }
 
-        public synchronized void SayHello(PolitePerson person) {
-            System.out.format("%s: %s has said Hello to me%n", this.name, person.getName());
-            person.SayHelloBack(this);
+        public void SayHello(PolitePerson person) {
+            bufferLock.lock();
+            try {
+                System.out.format("%s: %s has said Hello to me%n", this.name, person.getName());
+                person.SayHelloBack(this);
+            } finally {
+                bufferLock.unlock();
+            }
+
         }
 
-        public synchronized void SayHelloBack(PolitePerson person) {
-            System.out.format("%s: %s has said Hello back to me%n", this.name, person.getName());
+        public void SayHelloBack(PolitePerson person) {
+            bufferLock.lock();
+            try {
+                System.out.format("%s: %s has said Hello back to me%n", this.name, person.getName());
+            } finally {
+                bufferLock.unlock();
+            }
+
         }
     }
 }
