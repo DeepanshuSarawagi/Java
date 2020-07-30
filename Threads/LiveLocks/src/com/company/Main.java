33a6c0ba3a0d768f04e0bff8b2ea354c8a18801f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-30 10:51:08 +0530

    Create Worker class and define multiple methods to learn about LiveLocks

--- Threads/LiveLocks/src/com/company/Main.java
+++ Threads/LiveLocks/src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
 public class Main {
-
+    /**
+     * A LiveLock is similar to a DeadLock except that it is waiting for other blocked threads to finish executing.
+     * These threads are dependent on other threads to complete their tasks so that they can finish executing.
+     *
+     *
+     */
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ Threads/LiveLocks/src/com/company/SharedResource.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class SharedResource {
+    private Worker owner;
+
+}
