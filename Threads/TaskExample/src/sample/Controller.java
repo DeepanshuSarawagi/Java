f4b4da1e1cfa83de89fb1398809094bdaf194943
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-07-31 10:54:32 +0530

    Add the initialize() method in the Controller

--- Threads/TaskExample/src/sample/Controller.java
+++ Threads/TaskExample/src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+
 public class Controller {
+
+    private Task<ObservableList<String>> task;
+
+    public void initialize() {
+        task = new Task<ObservableList<String>>() {
+            @Override
+            protected ObservableList<String> call() throws Exception {
+                Thread.sleep(1000);
+                return FXCollections.observableArrayList(
+                        "Deepanshu Sarawagi",
+                             "Divya Sarawagi",
+                             "Rajat Sarawagi",
+                             "Pradeep Sarawagi",
+                             "Sangeetha Rani"
+                );
+            }
+        };
+    }
 }
