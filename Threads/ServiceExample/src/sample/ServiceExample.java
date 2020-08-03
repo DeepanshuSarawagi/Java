3759f90654caa69b3b941b9ea41df6ddf75931c2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-03 13:08:22 +0530

    Return new Task instance and implement the call() method

--- Threads/ServiceExample/src/sample/ServiceExample.java
+++ Threads/ServiceExample/src/sample/ServiceExample.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
@@ -7,6 +8,23 @@ import javafx.concurrent.Task;
 public class ServiceExample extends Service<ObservableList<String>> {
     @Override
     protected Task<ObservableList<String>> createTask() {
-        return null;
+        return new Task<ObservableList<String>>() {
+            @Override
+            protected ObservableList<String> call() throws Exception {
+                String[] names = {"Pradeep Sarawagi",
+                                  "Sangeetha Rani",
+                                  "Deepanshu Sarawagi",
+                                  "Divya Sarawagi",
+                                  "Rajat Sarawagi"};
+                ObservableList<String> employees = FXCollections.observableArrayList();
+                for (String name : names) {
+                    employees.add(name);
+                    updateMessage("Added " + name + " to the list");
+                    updateProgress(1, names.length);
+                    Thread.sleep(200);
+                }
+                return employees;
+            }
+        };
     }
 }
