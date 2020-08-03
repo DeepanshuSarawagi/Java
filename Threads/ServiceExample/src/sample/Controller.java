e6eff4c74669aa6647bc69c407b0375bace138e8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-03 13:13:46 +0530

    create new Service instance and bind it with listview`s items property

--- Threads/ServiceExample/src/sample/Controller.java
+++ Threads/ServiceExample/src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
@@ -7,4 +9,16 @@ public class Controller {
 
     @FXML
     private ListView<String> listView;
+
+    private Service<ObservableList<String>> service;
+
+    public void initialize() {
+        service = new ServiceExample();
+        listView.itemsProperty().bind(service.valueProperty());
+    }
+
+    @FXML
+    public void buttonPressed() {
+        service.start();
+    }
 }
