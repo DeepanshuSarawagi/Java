41b4d2345351eb1f2e5a3421fdb3c96d16a4936d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-03 13:16:25 +0530

    Create a progress bar and bind it with service's progress property

--- Threads/ServiceExample/src/sample/Controller.java
+++ Threads/ServiceExample/src/sample/Controller.java
@@ -4,17 +4,22 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
     @FXML
     private ListView<String> listView;
 
+    @FXML
+    private ProgressBar progressBar;
+
     private Service<ObservableList<String>> service;
 
     public void initialize() {
         service = new ServiceExample();
         listView.itemsProperty().bind(service.valueProperty());
+        progressBar.progressProperty().bind(service.progressProperty());
     }
 
     @FXML
