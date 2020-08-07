006f3d8809972bf43a1c5b2f7456398d9707e850
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-07 21:56:56 +0530

    Create private fields and constructor

--- Threads/Challenge2/src/com/company/BankAccount.java
+++ Threads/Challenge2/src/com/company/BankAccount.java
@@ -1,5 +1,11 @@
 package com.company;
 
 public class BankAccount {
+    private String AccountNumber;
+    private double Balance;
 
+    public BankAccount(String accountNumber, double balance) {
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
 }
