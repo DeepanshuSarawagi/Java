9d221de790f96ecfc18f42cef0411a3103a57eab
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-13 22:16:15 +0530

    Use the Lambda Expressions to do the same work

--- LambdaExpressions/Basics/src/com/company/Main.java
+++ LambdaExpressions/Basics/src/com/company/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 //        new Thread(new CodeToRun()).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Printing from the Runnable");
-            }
-        }).start();
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the Runnable");
+//            }
+//        }).start();
+
+        // Commented out above code to use Lambda Expressions in below example
+        new Thread(()-> System.out.println("Printing from the Runnable")).start();
     }
 }
 
