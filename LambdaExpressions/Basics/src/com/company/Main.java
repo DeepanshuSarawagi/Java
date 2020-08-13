b274a35213621e63f81a59d7dc5d5230bbfc460c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-13 22:19:38 +0530

    Understand more about Lambda

--- LambdaExpressions/Basics/src/com/company/Main.java
+++ LambdaExpressions/Basics/src/com/company/Main.java
@@ -14,6 +14,12 @@ public class Main {
 
         // Commented out above code to use Lambda Expressions in below example
         new Thread(()-> System.out.println("Printing from the Runnable")).start();
+        /*
+        Every Lambda Expression consists of three parts.
+            1. Argument list - ()
+            2. Error Token - ->
+            3. Body  - System.out.println("Printing from the Runnable")
+         */
     }
 }
 
