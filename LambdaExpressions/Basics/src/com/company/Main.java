ed1559fd6efb978b0a8f0f7e03eee048cdad238c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-13 22:24:53 +0530

    Understand more about Lambda

--- LambdaExpressions/Basics/src/com/company/Main.java
+++ LambdaExpressions/Basics/src/com/company/Main.java
@@ -19,6 +19,13 @@ public class Main {
             1. Argument list - ()
             2. Error Token - ->
             3. Body  - System.out.println("Printing from the Runnable")
+
+        How does the Lambda Expression evaluate the above code? In this case, the Lambda Expressions checks for the
+        parameter which is accepted by the Thread class constructor which is the Runnable interface. And this
+        Runnable interface implements just one method which is run() and it has no arguments. Hence, the empty
+        Argument List is evaluated against the Runnable Interface.
+        Since the Lambda Expressions looks out for methods, it can only be replaced against the interface with just one
+        method, also referred to as Functional Interfaces.
          */
     }
 }
