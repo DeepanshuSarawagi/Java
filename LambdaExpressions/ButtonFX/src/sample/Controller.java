eb07a95e0189f18b98c7436e103e7b9432d6fb1d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-14 21:26:56 +0530

    Simplify the intialize() method using Lambda Expression

--- LambdaExpressions/ButtonFX/src/sample/Controller.java
+++ LambdaExpressions/ButtonFX/src/sample/Controller.java
@@ -10,13 +10,19 @@ public class Controller {
     @FXML
     private Button clickMeButton;
 
+//    public void initialize() {
+//        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                System.out.println("You clicked me!!");
+//            }
+//        });
+//    }
+
+    // Commented out above code to see the example of Lambda Expression and how it simplifies above code
+
     public void initialize() {
-        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("You clicked me!!");
-            }
-        });
+        clickMeButton.setOnAction(actionEvent -> System.out.println("You clicked me!!"));
     }
 
 }
