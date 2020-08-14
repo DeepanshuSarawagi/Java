6588a1ab01ae856375fa3360124d1037a140aab1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-14 21:23:52 +0530

    Set the action handler for the button when its clicked

--- LambdaExpressions/ButtonFX/src/sample/Controller.java
+++ LambdaExpressions/ButtonFX/src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -7,5 +9,14 @@ public class Controller {
 
     @FXML
     private Button clickMeButton;
+
+    public void initialize() {
+        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("You clicked me!!");
+            }
+        });
+    }
 
 }
