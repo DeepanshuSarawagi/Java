d6fd80ee5c867742ae66202343892db9fe98354f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-14 21:46:10 +0530

    Implement the interface and test the code

--- LambdaExpressions/Basics/src/com/company/Main.java
+++ LambdaExpressions/Basics/src/com/company/Main.java
@@ -75,6 +75,19 @@ public class Main {
         for (Employee employee: employees) {
             System.out.println(employee.getName());
         }
+
+        String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        }, employees.get(0).getName(), employees.get(1).getName());
+
+        System.out.println(sillyString);
+    }
+
+    public static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -104,5 +117,5 @@ class Employee {
 }
 
 interface UpperConcat {
-    public String upperAndConcat(String s1, String s2);
+    String upperAndConcat(String s1, String s2);
 }
