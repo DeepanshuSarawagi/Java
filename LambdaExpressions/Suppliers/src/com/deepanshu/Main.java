35e286a16dbf0f188ab06c5e79034e026bf077bc
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-15 20:20:14 +0530

    Learn about Supplier Interface

---
+++ LambdaExpressions/Suppliers/src/com/deepanshu/Main.java
@@ -0,0 +1,19 @@
+package com.deepanshu;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+/**
+ * In this example, we are going to learn about Supplier. Supplier is yet another functional interface which returns the
+ * value of type T. Hence, we use the Supplier.get() method to get the value.
+ * In the below example, we have used the Supplier<Integer> interface to print the 10 random numbers.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt(100);
+        for (int i=0; i<10; i++) {
+            System.out.println(randomSupplier.get());
+        }
+    }
+}
