3755bee9951a032bc2ad3bcd6b44328eaf026134
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-15 16:44:15 +0530

    Create Employees and sort them using names

--- LambdaExpressions/ScopeAndFunctinalProgramming/src/com/company/Main.java
+++ LambdaExpressions/ScopeAndFunctinalProgramming/src/com/company/Main.java
@@ -1,9 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        Employee pradeep = new Employee("Pradeep Sarawagi", 57);
+        Employee sangeetha = new Employee("Sangeetha Rani", 53);
+        Employee deepanshu = new Employee("Deepashu Sarawagi", 27);
+        Employee divya = new Employee("Divya Mittal", 27);
+        Employee rajat = new Employee("Rajat Sarawagi", 25);
 
+        List<Employee> employees = new ArrayList<>();
+        employees.add(pradeep);
+        employees.add(sangeetha);
+        employees.add(deepanshu);
+        employees.add(divya);
+        employees.add(rajat);
+
+        Collections.sort(employees, (employee1, employee2) -> employee1.getName().compareTo(employee2.getName()));
+
+        for (Employee employee : employees) {
+            System.out.println(employee.getName());
+        }
     }
 }
 
