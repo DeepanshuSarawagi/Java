022e0acc0db10da04c0ee11fb2dbdba17ca2680a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-15 18:36:32 +0530

    repeat the code from previous lecture

--- LambdaExpressions/FunctionalInterfacesAndPredicates/src/com/company/Employee.java
+++ LambdaExpressions/FunctionalInterfacesAndPredicates/src/com/company/Employee.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Employee {
+    private String Name;
+    private int Age;
+
+    public Employee(String name, int age) {
+        Name = name;
+        Age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
 }
--- LambdaExpressions/FunctionalInterfacesAndPredicates/src/com/company/Main.java
+++ LambdaExpressions/FunctionalInterfacesAndPredicates/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        Employee pradeep = new Employee("Pradeep Sarawagi", 57);
+        Employee sangeetha = new Employee("Sangeetha Rani", 53);
+        Employee deepanshu = new Employee("Deepanshu Sarawagi", 27);
+        Employee divya = new Employee("Divya Sarawagi", 27);
+        Employee rajat = new Employee("Rajat Sarawagi", 25);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(pradeep);
+        employees.add(sangeetha);
+        employees.add(deepanshu);
+        employees.add(divya);
+        employees.add(rajat);
 
     }
 }
