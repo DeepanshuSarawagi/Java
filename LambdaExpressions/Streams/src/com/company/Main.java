5d7009778ee3fddf385e4beae9792117157bce98
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-18 17:25:25 +0530

    Learn how to Streams on a Collections object

--- LambdaExpressions/Streams/src/com/company/Main.java
+++ LambdaExpressions/Streams/src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -40,6 +41,24 @@ public class Main {
 
         gNumbers.sort((String o1, String o2) -> o1.compareTo(o2));
         gNumbers.forEach(s -> System.out.println(s));
+
+        /*
+        Now we are going to use Streams to achieve the same result as above in a single line of code
+        A Stream is a sequence of elements supporting sequential and parallel operations.
+        In the below examples, we have applied the Stream method to the source Collections object. And then we have
+        asked the stream to convert all the objects in the collections to UpperCase, followed by a filter to filter the
+        objects whose string starts with G and then calling the sorted() method to sort the items which are filtered and
+        finally we print each element using the forEach() method.
+        Note that, the original Collections someBingoNumbers is not manipulated. Every set of operation is independent.
+        No changes will be made to the original list.
+         */
+        System.out.println();
+
+        someBingoNumbers.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println);
 
     }
 }
