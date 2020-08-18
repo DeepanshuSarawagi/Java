c1e03efaadf517d0c6f597daa8e4c0422458fd32
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-18 17:04:12 +0530

    print the sorted number string which starts with G

--- LambdaExpressions/Streams/src/com/company/Main.java
+++ LambdaExpressions/Streams/src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> someBingoNumbers = Arrays.asList("N40", "N36",
-                "G53", "G71", "G11",
+                "G53", "G71", "G11", "g64",
                 "I26", "I70",
                 "B5", "B29", "B16");
 
@@ -24,5 +27,19 @@ public class Main {
                 System.out.print(number + "\t");
             }
         });
+
+        // Create a new ArrayList
+        List<String> gNumbers = new ArrayList<>();
+
+        someBingoNumbers.forEach(number -> {
+            if (number.toUpperCase().startsWith("G")) {
+                gNumbers.add(number);
+            }
+        });
+        System.out.println();
+
+        gNumbers.sort((String o1, String o2) -> o1.compareTo(o2));
+        gNumbers.forEach(s -> System.out.println(s));
+
     }
 }
