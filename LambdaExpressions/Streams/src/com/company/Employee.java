369185c518ac4d3898c631143d605b6e35b0f61a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-19 17:30:48 +0530

    Create a basic Employee class

---
+++ LambdaExpressions/Streams/src/com/company/Employee.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Employee {
+    private String name;
+    private String age;
+
+    public Employee(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
