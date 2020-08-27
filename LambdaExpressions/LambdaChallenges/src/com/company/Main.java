f579600705ba7198900e65e4e346e67590cd5e89
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-27 12:56:53 +0530

    Convert the code into Function using Lambda

--- LambdaExpressions/LambdaChallenges/src/com/company/Main.java
+++ LambdaExpressions/LambdaChallenges/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 public class Main {
 
@@ -12,5 +13,18 @@ public class Main {
                     .forEach(System.out::println);
         };
 	    runnable.run();
+
+        Function<String, String> lambdaString = s -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i=0; i<s.length(); i++) {
+                if (i % 2 ==1) {
+                    stringBuilder.append(s.charAt(i));
+                    stringBuilder.append(" ");
+                }
+            }
+            return stringBuilder.toString();
+        };
+
+        System.out.println(lambdaString.apply("1234567890"));
     }
 }
