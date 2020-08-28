27587aa37ca6e8ee8715809e2f1c87998a9a6d47
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-28 21:55:50 +0530

    Basics of regular expressions

--- RegularExpressions/Basics/src/com/company/Main.java
+++ RegularExpressions/Basics/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String string = "I am a String. Yes, I am!";
+        System.out.println(string);
 
+        // Convert all the occurrences of "I" with "You"
+
+        String youString = string.replace("I", "You");
+        System.out.println(youString);
     }
 }
