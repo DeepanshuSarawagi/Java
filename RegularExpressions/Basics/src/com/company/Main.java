6c4892a2543709f01ea4f73cd9111bdf09489251
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-28 21:59:52 +0530

    Use the Character class Regex to replace the entire string with a String

--- RegularExpressions/Basics/src/com/company/Main.java
+++ RegularExpressions/Basics/src/com/company/Main.java
@@ -10,5 +10,10 @@ public class Main {
 
         String youString = string.replace("I", "You");
         System.out.println(youString);
+
+        // We are going to Character Class to replace the entire string with "Y"
+
+        String alphaNumeric = "abcDeeeFG20FghiiiiJKL90xyZ";
+        System.out.println(alphaNumeric.replaceAll(".", "Y"));
     }
 }
