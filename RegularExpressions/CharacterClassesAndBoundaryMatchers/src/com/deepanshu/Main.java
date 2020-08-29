2f2c3873a2b7a1d6898f02c385893cba7fe75b16
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-29 22:46:54 +0530

    Learn how to replace a word with Upper/Lower case first letter

---
+++ RegularExpressions/CharacterClassesAndBoundaryMatchers/src/com/deepanshu/Main.java
@@ -0,0 +1,10 @@
+package com.deepanshu;
+
+public class Main {
+    public static void main(String[] args) {
+        // In the below example we will see how to replace the word with Upper/Lower case first letter with another
+        // word
+
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+    }
+}
