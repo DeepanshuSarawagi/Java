f0140117bd9b715603b6eac893c4c22ce34e1316
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-30 22:53:26 +0530

    Learn how to use Quantifiers in RegEx

--- RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Main.java
+++ RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        String alphaNumeric = "abcDeeeeFg111hiiiiiJk910Xy";
+        System.out.println(alphaNumeric.replaceAll("^abcDe{4}", "X"));
+        // We use the quantifiers in the regEx. It is helpful to specify the exact count of preceding character which
+        // match in the String
     }
 }
