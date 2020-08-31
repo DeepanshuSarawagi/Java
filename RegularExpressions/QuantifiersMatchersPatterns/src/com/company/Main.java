7e9acfa36ea44be20169764a20834c812401a418
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-31 22:14:46 +0530

    Use the +(Plus) quantifier to achieve the same result

--- RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Main.java
+++ RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Main.java
@@ -6,6 +6,12 @@ public class Main {
         String alphaNumeric = "abcDeeeeFg111hiiiiiJk910Xy";
         System.out.println(alphaNumeric.replaceAll("^abcDe{4}", "X"));
         // We use the quantifiers in the regEx. It is helpful to specify the exact count of preceding character which
-        // match in the String
+        // match in the String.
+        // A Quantifier specifies how often an element in a regular expression can occur
+
+        // In the below example we will use the + quantifier which requires us not to specify the exact number of
+        // occurrences of an element in the regular expression
+
+        System.out.println(alphaNumeric.replaceAll("^abcDe+", "X"));
     }
 }
