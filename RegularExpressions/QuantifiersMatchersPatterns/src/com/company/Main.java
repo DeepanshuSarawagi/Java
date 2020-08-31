26d8cbbaffd14fe7caca08c74c7896da2a1b1baf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-31 22:17:48 +0530

    Learn the purpose of using the asterisk quantifier

--- RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Main.java
+++ RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Main.java
@@ -13,5 +13,10 @@ public class Main {
         // occurrences of an element in the regular expression
 
         System.out.println(alphaNumeric.replaceAll("^abcDe+", "X"));
+
+        // Supposedly if we dont want to specify the number of e's however, we want to ensure that it should be
+        // followed by 1 or more e's then we need to use the asterisk quantifier
+
+        System.out.println(alphaNumeric.replaceAll("abcDe*", "X"));
     }
 }
