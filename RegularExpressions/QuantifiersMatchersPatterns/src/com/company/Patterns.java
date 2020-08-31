dc71ed716db85c0a8c7a9b508dd3d03f0659880e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-08-31 22:39:11 +0530

    Learn how to Patterns and Matcher to match a recurring pattern in the String

---
+++ RegularExpressions/QuantifiersMatchersPatterns/src/com/company/Patterns.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Patterns {
+    public static void main(String[] args) {
+        StringBuilder htmlText = new StringBuilder("<h1>My heading</h1>");
+        htmlText.append("<h2>Sub-Heading</h2>");
+        htmlText.append("<p>This is a paragraph about something!");
+        htmlText.append("<h2>Another Sub-Heading</h2>");
+        htmlText.append("<p>This is another paragraph about something else</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>This is the Summary</p>");
+
+        System.out.println(htmlText);
+
+        String h2Pattern = "<h2>";   //
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
+    }
+}
