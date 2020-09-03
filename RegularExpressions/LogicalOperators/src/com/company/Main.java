97142f5f1737a08231992ca8ccb363c6ec9f3682
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-03 22:35:18 +0530

    Use logical operators in the RegEx

--- RegularExpressions/LogicalOperators/src/com/company/Main.java
+++ RegularExpressions/LogicalOperators/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        // Use the logical operators in the RegEx
+        System.out.println("harry".replaceAll("[H|h]arry", "Larry"));
     }
 }
