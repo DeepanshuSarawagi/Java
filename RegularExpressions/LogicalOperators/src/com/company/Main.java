5b6337a1020ddf718d4aff50f95adefdc67705e4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-04 23:23:14 +0530

    Learn more about logical operators

--- RegularExpressions/LogicalOperators/src/com/company/Main.java
+++ RegularExpressions/LogicalOperators/src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
         // Use the logical operators in the RegEx
         System.out.println("harry".replaceAll("[H|h]arry", "Larry"));
+        String string = "IHaveAString";
+        Pattern pattern = Pattern.compile("[a|A]");
+        Matcher matcher = pattern.matcher(string);
+
+        if (matcher.matches()) {
+            System.out.println(string.replaceAll("[^a|A]", "YY"));
+        }
     }
 }
