c9fdfb66df5f919fdf0d3bc78a38ad84f85eb2b5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-07 20:25:25 +0530

    Complete challenge2.

--- RegularExpressions/Challenges/src/com/company/Main.java
+++ RegularExpressions/Challenges/src/com/company/Main.java
@@ -6,7 +6,10 @@ public class Main {
 	    String string1 = "I want a bike.";
 
         System.out.println(string1.matches("I want a bike."));
-
+        String regEx = "I want a \\w+.";
+        System.out.println(string1.matches(regEx));
+        String challenge2 = "I want a ball.";
+        System.out.println(challenge2.matches(regEx));
         System.out.println(string1.replaceAll("bike.$", "ball."));
     }
 }
