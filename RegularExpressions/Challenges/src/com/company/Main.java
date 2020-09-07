d80fe3b858c7a30035a79218ab129c01a2623f35
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-07 20:19:34 +0530

    Complete challenge 1

---
+++ RegularExpressions/Challenges/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String string1 = "I want a bike.";
+
+        System.out.println(string1.matches("I want a bike."));
+    }
+}
