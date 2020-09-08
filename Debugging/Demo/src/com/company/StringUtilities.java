c049479deeae426f3e3e3183760fa7ad975fff83
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-08 18:03:15 +0530

    Learn about watchers/watch points in debugger

--- Debugging/Demo/src/com/company/StringUtilities.java
+++ Debugging/Demo/src/com/company/StringUtilities.java
@@ -5,6 +5,7 @@ public class StringUtilities {
     private int charsAdded = 0;
     public void addChar(StringBuilder sBuilder, char c) {
         sBuilder.append(c);
-        charsAdded++;
+        charsAdded++;   // When we run the debugger we will see that this variable is a watch point which helps us to
+        // test the code
     }
 }
