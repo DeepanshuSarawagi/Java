ccaf464c9d9030c1e07fd7e1641a5ee19ac1cf46
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-08 21:01:18 +0530

    Create a BankAccount class and create the constructor with all three fields

--- Debugging/JUnitTestingExample/src/com/company/BankAccount.java
+++ Debugging/JUnitTestingExample/src/com/company/BankAccount.java
@@ -1,5 +1,15 @@
 package com.company;
 
 public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
 
 }
