8efe83d6bd868f28b44e2c711ef3ec10fdc89056
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-11 11:06:46 +0530

    Create new UtilitiesTest class for unit testing using JUnit5

---
+++ Debugging/JUnitChallenges/src/com/company/UtilitiesTest.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilitiesTest {
+
+    @org.junit.jupiter.api.Test
+    void everyNthChar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removePairs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void converter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nullIfOddLength() {
+    }
+}
