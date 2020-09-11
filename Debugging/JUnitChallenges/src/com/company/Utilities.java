ca66186ae76c5750e15d850ce263d45524203ad9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-11 11:00:38 +0530

    Create one more new method for challenges

--- Debugging/JUnitChallenges/src/com/company/Utilities.java
+++ Debugging/JUnitChallenges/src/com/company/Utilities.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Utilities {
 
+    // Returns a char array containing every nth char. When sourceArray.length < n, return sourceArray
     public char[] everyNthChar(char[] sourceArray, int n) {
         if (sourceArray == null || sourceArray.length < n) {
             return sourceArray;
@@ -16,5 +17,21 @@ public class Utilities {
         return result;
     }
 
+    // Remove pairs of the same character that are next to each other
+    // For e.g; "ABBCDEEF" -> "ABCDEF"
+    public String removePairs(String source) {
+        // If length is < 2, there wont be any pairs.
+        if (source.length() < 2) {
+            return source;
+        }
 
+        StringBuilder sb = new StringBuilder();
+        char[] string = source.toCharArray();
+        for (int i =0; i<string.length; i++) {
+            if (string[i] != string[i++]) {
+                sb.append(string[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
