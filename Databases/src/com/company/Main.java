5bbe0bab4373f8b293c9be8feed2589c18d2b311
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-15 22:19:43 +0530

    Use the try with resources block to close the JDBC connection when no longer in use

--- Databases/src/com/company/Main.java
+++ Databases/src/com/company/Main.java
@@ -8,9 +8,8 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) {
-	    try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/deepanshusarawagi/Desktop/Learning/Java/Databases/src/testJava.db");
-            Statement statement = conn.createStatement();
+	    try(Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/deepanshusarawagi/Desktop/Learning/Java/Databases/src/testJava.db");
+            Statement statement = conn.createStatement();) {
             statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
         } catch (SQLException e) {
             System.out.println("Something went wrong! " + e.getMessage());
