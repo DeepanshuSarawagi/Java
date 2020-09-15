5344c4c7eec5b5b8a69647fce855920a0cb517d8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-15 22:06:23 +0530

    Use the JDBC driver and DriverManager class to get the JDBC connection

--- Databases/src/com/company/Main.java
+++ Databases/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite://Users/deepanshusarawagi/Desktop/Learning/Java/Databases/src/testJava.db");
+        } catch (SQLException e) {
+            System.out.println("Something went wrong! " + e.getMessage());
+        }
     }
 }
