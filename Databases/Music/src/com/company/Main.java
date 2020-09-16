a760c656514bd88898ad6533695297fdecfa2a2e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-16 12:42:16 +0530

    Test the datasource connection by creating instance of DataSource class

--- Databases/Music/src/com/company/Main.java
+++ Databases/Music/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.company.Model.Datasource;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Datasource datasource = new Datasource();
+        if (!datasource.open()) {
+            System.out.println("Couldn't open datasource");
+            return;
+        }
+        datasource.close();
     }
 }
