a56151d4ba5f751cf83e0a69ffafef280f35c9d2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-17 12:13:21 +0530

    Create new SongArtist class to save the Instance's objects in the fields

--- Databases/Music/src/com/company/Main.java
+++ Databases/Music/src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", " + "Name = " + artist.getName());
         }
 
-        List<String> albumsForArtist = datasource.queryAlbumsForArtists("Pink Floyd", Datasource.ORDER_BY_ASC);
+        List<String> albumsForArtist = datasource.queryAlbumsForArtists("Carole King", Datasource.ORDER_BY_ASC);
         for (String album : albumsForArtist) {
             System.out.println(album);
         }
