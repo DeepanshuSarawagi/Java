7c4d404eb0300b6e7aa8c291d04f41dfdf44ce2c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-09-27 09:23:22 +0530

    Create insertAlbums() method to insert record into Albums table

--- Databases/Music/src/com/company/Model/Datasource.java
+++ Databases/Music/src/com/company/Model/Datasource.java
@@ -398,4 +398,26 @@ public class Datasource {
             }
         }
     }
+
+    private int insertAlbum(String name) throws SQLException {
+        queryAlbum.setString(1, name);
+        ResultSet results = queryAlbum.executeQuery();
+        if (results.next()) {
+            return results.getInt(1);
+        } else {
+            insertIntoAlbums.setString(1, name);
+            int affectedRows = insertIntoAlbums.executeUpdate();
+
+            if (affectedRows != 1) {
+                throw new SQLException("Couldn't insert album: " + name);
+            }
+
+            ResultSet generatedKeys = insertIntoAlbums.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                return generatedKeys.getInt(1);
+            } else {
+                throw new SQLException("Couldn't get _id of the album");
+            }
+        }
+    }
 }
