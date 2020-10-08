eb038555d44e890cd49ca9eacc2d32c4c1f2ffc2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-08 23:36:31 +0530

    Create private fields

--- Databases/MusicBrowser/src/sample/Controller.java
+++ Databases/MusicBrowser/src/sample/Controller.java
@@ -1,5 +1,12 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    private GridPane gridPane;
+
+    private Button button;
 
 }
