a15dba7519d0999d1f7c7fef9833067513e04d63
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-19 22:50:40 +0530

    Create BroderPane with TableView and columns

--- Databases/MusicBrowser/src/sample/Controller.java
+++ Databases/MusicBrowser/src/sample/Controller.java
@@ -1,25 +1,4 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.GridPane;
-
 public class Controller {
-
-    @FXML
-    private GridPane gridPane;
-
-    @FXML
-    private Button button;
-
-    private Button button2;
-
-    private ListView listView;
-
-    private Label label;
-
-    private Label label1;
-
 }
