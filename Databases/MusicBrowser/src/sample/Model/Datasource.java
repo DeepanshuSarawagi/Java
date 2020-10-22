ba202befa9aabf3a79f0161087ac693aadba23ed
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-22 23:08:20 +0530

    Fix the typo in the connection string

--- Databases/MusicBrowser/src/sample/Model/Datasource.java
+++ Databases/MusicBrowser/src/sample/Model/Datasource.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
-    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/deepanshusarawagi/Desktop/Learning/Java/Databases/MusicBrowser" + DB_NAME;
+    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/deepanshusarawagi/Desktop/Learning/Java/Databases/MusicBrowser/" + DB_NAME;
 
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
