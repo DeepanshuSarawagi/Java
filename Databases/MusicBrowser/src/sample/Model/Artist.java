a8cf11ce11fa59517ec907ea9277c8afbcceb6a6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-23 19:59:32 +0530

    Fix the getters of Artist class

--- Databases/MusicBrowser/src/sample/Model/Artist.java
+++ Databases/MusicBrowser/src/sample/Model/Artist.java
@@ -2,21 +2,22 @@ package sample.Model;
 
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringPropertyBase;
 
 public class Artist {
     private SimpleIntegerProperty id;  // Change the field type from int to SimpleIntegerProperty for data binding in future
     private SimpleStringProperty name;  // Change the field type from int to SimpleStringProperty for data binding in future
 
-    public SimpleIntegerProperty getId() {
-        return id;
+    public int getId() {
+        return id.get();
     }
 
     public void setId(SimpleIntegerProperty id) {
         this.id = id;
     }
 
-    public SimpleStringProperty getName() {
-        return name;
+    public String getName() {
+        return name.get();
     }
 
     public void setName(SimpleStringProperty name) {
