92c7165f1a27518c27aec57f5bf7494df25b2909
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-23 19:22:02 +0530

    Create a new class which will return the ObservableList of Artist objects

--- Databases/MusicBrowser/src/sample/Controller.java
+++ Databases/MusicBrowser/src/sample/Controller.java
@@ -1,7 +1,20 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.scene.layout.BorderPane;
+import sample.Model.Artist;
+import sample.Model.Datasource;
 
 public class Controller {
 
+
+}
+
+class GetAllArtistsTask extends Task {
+    @Override
+    public ObservableList<Artist> call() throws Exception {
+        return FXCollections.observableArrayList(Datasource.getInstance().queryArtist(Datasource.ORDER_BY_ASC));
+    }
 }
