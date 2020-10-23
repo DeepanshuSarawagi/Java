47f7127c8c7ed26080b2945426b86f3650cd99c5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-23 19:27:10 +0530

    Add the understanding of the code in comments for better readability in future

--- Databases/MusicBrowser/src/sample/Controller.java
+++ Databases/MusicBrowser/src/sample/Controller.java
@@ -15,6 +15,10 @@ public class Controller {
 class GetAllArtistsTask extends Task {
     @Override
     public ObservableList<Artist> call() throws Exception {
+        // Here, queryList(int sortorder) method returns the list of Artist objects. We are then using the
+        // FXCollections.observableArrayList to convert the list into ObservableArrayList. This helps us to separate the
+        // Java code from JavaFX code.
+
         return FXCollections.observableArrayList(Datasource.getInstance().queryArtist(Datasource.ORDER_BY_ASC));
     }
 }
