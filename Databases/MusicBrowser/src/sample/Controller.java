3422009d45978aa94e5441a6515c79d622335400
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-10-24 21:59:40 +0530

    Create new method to display the albums for selected artist in Controller class

--- Databases/MusicBrowser/src/sample/Controller.java
+++ Databases/MusicBrowser/src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+import sample.Model.Album;
 import sample.Model.Artist;
 import sample.Model.Datasource;
 
@@ -21,12 +22,30 @@ public class Controller {
         new Thread(task).start();  // Kick of the Task to load the Artist objects in TableColumn
     }
 
+    @FXML
+    public void ListAlbumsForArtist() {
+        final Artist artist = (Artist) artistTable.getSelectionModel().getSelectedItem();
+        if (artist == null) {
+            System.out.println("No Artist selected");
+            return;
+        }
+
+        Task<ObservableList<Album>> task = new Task<ObservableList<Album>>() {
+            @Override
+            protected ObservableList<Album> call() throws Exception {
+                return FXCollections.observableArrayList(Datasource.getInstance().queryAlbumForArtistId(artist.getId()));
+            }
+        };
+        artistTable.itemsProperty().bind(task.valueProperty());
+        new Thread(task).start();
+    }
+
 }
 
 class GetAllArtistsTask extends Task {
     @Override
     public ObservableList<Artist> call() throws Exception {
-        // Here, queryList(int sortorder) method returns the list of Artist objects. We are then using the
+        // Here, queryList(int sortOrder) method returns the list of Artist objects. We are then using the
         // FXCollections.observableArrayList to convert the list into ObservableArrayList. This helps us to separate the
         // Java code from JavaFX code.
 
