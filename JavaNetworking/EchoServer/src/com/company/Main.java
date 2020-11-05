ab1e2ca481c6b21edff8f9da2bc2ec0e224ef2a4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-05 20:34:49 +0530

    Understand how ServerSocket and Socket works in Java

--- JavaNetworking/EchoServer/src/com/company/Main.java
+++ JavaNetworking/EchoServer/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        try(ServerSocket serverSocket = new ServerSocket(5000)) {
+            Socket socket = serverSocket.accept();  // We block the ServerSocket until we have the server to listen to
+            // the clients on the port we have assigned. Hence, we use the ServerSocket.accept() method which returns
+            // the Socket instance. This will be the socket used to communicate with the client.
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
