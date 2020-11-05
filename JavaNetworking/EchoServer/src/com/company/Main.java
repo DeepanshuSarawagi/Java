cfddd7498ac0683a8521b4338aa61d20ae81e75e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-05 21:42:52 +0530

    Make changes in ServerSocket to accept multiple client connections

--- JavaNetworking/EchoServer/src/com/company/Main.java
+++ JavaNetworking/EchoServer/src/com/company/Main.java
@@ -11,22 +11,22 @@ public class Main {
 
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(5000)) {
-            Socket socket = serverSocket.accept();  // We block the ServerSocket until we have the server to listen to
-            // the clients on the port we have assigned. Hence, we use the ServerSocket.accept() method which returns
-            // the Socket instance. This will be the socket used to communicate with the client. The accept() method is
-            // going to block until the client connects to the server
-            System.out.println("Client connected");
+            while (true) {  // Creating sockets infinitely to accept multiple requests from client
 
-            // We use Input and Output Stream to send input to the client and receive data from the client. It is a
-            // common practice to wrap the input stream inside BufferedInputReader and output stream in PrintWriter.
+                Socket socket = serverSocket.accept();  // We block the ServerSocket until we have the server to listen to
+                // the clients on the port we have assigned. Hence, we use the ServerSocket.accept() method which returns
+                // the Socket instance. This will be the socket used to communicate with the client. The accept() method is
+                // going to block until the client connects to the server
+                System.out.println("Client connected");
 
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true); // the second parameter "true"
-            // is used to specify if we want to autoFlush the output stream
+                // We use Input and Output Stream to send input to the client and receive data from the client. It is a
+                // common practice to wrap the input stream inside BufferedInputReader and output stream in PrintWriter.
 
-            // Now we are going to read the input using InputStream and write the output using OutputStream
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true); // the second parameter "true"
+                // is used to specify if we want to autoFlush the output stream
 
-            while (true) {
+                // Now we are going to read the input using InputStream and write the output using OutputStream
                 String echoString = input.readLine();
                 if (echoString.equals("exit")) {
                     break;
