d9d2f0dab4f4c772f89c54c2dbc35539923f52fd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-08 20:10:47 +0530

    Print the received packet from the server on the UDP client terminal

--- JavaNetworking/UDPClient/src/com/company/Main.java
+++ JavaNetworking/UDPClient/src/com/company/Main.java
@@ -22,6 +22,12 @@ public class Main {
                 byte[] buffer = echoString.getBytes();
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5000);
                 datagramSocket.send(packet);
+
+                byte[] buffer2 = new byte[50];
+                packet = new DatagramPacket(buffer2, buffer2.length);
+                datagramSocket.receive(packet);
+                System.out.println(new String(buffer2, 0, packet.getLength()));
+
             } while (!echoString.equals("exit"));
 
         } catch (SocketTimeoutException e) {
