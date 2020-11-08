0b623d85b5663e4b68701241150136581727d871
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-08 20:32:37 +0530

    Learn how URI scheme and scheme specific part

---
+++ JavaNetworking/NetworkingAPIs/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Main {
+
+    /**
+     * In this lesson, we are going to learn about URI and URL and how they can be used in Java Networking.
+     * URI consists of following segments
+     * 1. Scheme
+     * 2. Scheme-Sepcific Part
+     * 3. Authority
+     * 4. User-info
+     * 5. Host
+     * 6. Port
+     * 7. Path
+     * 8. Query
+     * 9. Fragment
+     */
+
+    public static void main(String[] args) {
+        try {
+            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            System.out.println("Scheme = " + uri.getScheme());
+            System.out.println("Scheme Specific-part = " + uri.getSchemeSpecificPart());
+            System.out.println("Authority = " + uri.getAuthority());
+            System.out.println("User-Info " + uri.getUserInfo());
+            System.out.println("Host = " + uri.getHost());
+            System.out.println("Port = " + uri.getPort());
+            System.out.println("Path = " + uri.getPath());
+            System.out.println("Query = " + uri.getQuery());
+            System.out.println("Fragment = " + uri.getFragment());
+        } catch (URISyntaxException e) {
+            System.out.println("Bad URI syntax: " + e.getMessage());
+        }
+    }
+}
