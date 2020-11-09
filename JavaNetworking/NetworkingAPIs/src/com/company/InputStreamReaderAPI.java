7afa87252a7bc776c05f994bb6f4ad56d40977fc
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-09 22:59:14 +0530

    Learn how to read the contents of a web page using URL and InputStreamReaderAPI

---
+++ JavaNetworking/NetworkingAPIs/src/com/company/InputStreamReaderAPI.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class InputStreamReaderAPI {
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://example.org");
+
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
+        } catch (MalformedURLException e) {
+            System.out.println("URL Malformed error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
