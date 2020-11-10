adf65e57888e67fec003febf46a7c867c90a0ccd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-10 22:35:45 +0530

    Read the contents of the web page using URLConnection class

--- JavaNetworking/NetworkingAPIs/src/com/company/URLConnectionExample.java
+++ JavaNetworking/NetworkingAPIs/src/com/company/URLConnectionExample.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -12,8 +14,17 @@ public class URLConnectionExample {
         try{
             URL url = new URL("http://example.org");
             URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
             urlConnection.connect();  // We call the URLConnection.connect() method to connect to the URL thats because
             // the openConnection() call doesnt connect to the URL
+
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
