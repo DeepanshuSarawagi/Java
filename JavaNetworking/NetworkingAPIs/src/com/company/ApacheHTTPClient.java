36c73fce7da14683042ec849facc2794f992ab85
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-11 23:02:52 +0530

    Use ApacheHTTPClient to read the contents of webpage

--- JavaNetworking/NetworkingAPIs/src/com/company/ApacheHTTPClient.java
+++ JavaNetworking/NetworkingAPIs/src/com/company/ApacheHTTPClient.java
@@ -2,7 +2,13 @@ package com.company;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class ApacheHTTPClient {
     public static void main(String[] args) {
@@ -10,6 +16,27 @@ public class ApacheHTTPClient {
         HttpGet request = new HttpGet("http://example.org");  // Create a HTTP Get request
         request.addHeader("User-Agent", "Chrome");  // Add the header
 
+        CloseableHttpResponse response = null;
 
+        try {
+            response = httpClient.execute(request);
+            System.out.println("response code = " + response.getCode());
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line;
+
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inputReader.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
     }
 }
