f1e06d2647fbb9e15e8dbff0bd97e907c7d6d78b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-11 21:23:29 +0530

    Setting the RequestMethod of URL connection to GET method

--- JavaNetworking/NetworkingAPIs/src/com/company/HTTPURLConnectionExample.java
+++ JavaNetworking/NetworkingAPIs/src/com/company/HTTPURLConnectionExample.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HTTPURLConnectionExample {
+    public static void main(String[] args) {
+        try{
+            URL url = new URL("http://example.org");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");  // Setting the RequestMethod to GET method
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
