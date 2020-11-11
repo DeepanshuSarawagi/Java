a80d1774985d6b7450e0b171f91581db439128c8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-11 22:52:14 +0530

    Create a default HTTP GET Request using CloseableHttpClient class

--- JavaNetworking/NetworkingAPIs/src/com/company/ApacheHTTPClient.java
+++ JavaNetworking/NetworkingAPIs/src/com/company/ApacheHTTPClient.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 public class ApacheHTTPClient {
     public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();  // Create a default HTTP client
+        HttpGet request = new HttpGet("http://example.org");  // Create a HTTP Get request
+        request.addHeader("User-Agent", "Chrome");  // Add the header
+
 
     }
 }
