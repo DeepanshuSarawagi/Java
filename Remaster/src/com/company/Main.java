cf517e6d8d14f16ebb28bc647845e8d1d30db70d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-13 21:46:04 +0530

    Operators associativity

---
+++ Remaster/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    /**
+     * An expression is a construct that evaluates to a single value.
+     */
+
+    public static void main(String[] args) {
+        // Operators associativity example
+        double kilometers = (100d * 1.603944d);
+        System.out.println("Kilometers = " + kilometers);
+
+    }
+}
