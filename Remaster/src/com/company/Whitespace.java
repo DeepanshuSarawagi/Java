cd8e21e78035e2d7655456a434c28fab2adfc652
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-18 20:40:33 +0530

    Learn about Whitespace and indentation in Java

---
+++ Remaster/src/com/company/Whitespace.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Whitespace {
+    public static void main(String[] args) {
+
+        System.out.println("This is a nice chunk " +
+                "of text!");
+    }
+}
