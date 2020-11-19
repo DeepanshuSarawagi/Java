06d88cd750ff69a4f6fdacabb87dac51fae521f9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-19 21:23:12 +0530

    Create new project to recap about Methods

---
+++ Methods/Recap/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    /**
+     * The general form of defining a method is
+     * <Access Modifier><Return Type><Method Name><(Parameter)> {
+     *
+     * }
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
