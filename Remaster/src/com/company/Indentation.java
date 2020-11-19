61043b4a4a5d6da41a2e3dc7472137c92095e0e4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-19 20:29:10 +0530

    Understand about code-blocks in Java

--- Remaster/src/com/company/Indentation.java
+++ Remaster/src/com/company/Indentation.java
@@ -1,10 +1,16 @@
 package com.company;
 
 public class Indentation {
-    public void indentationExample() {
+    protected void indentationExample() {
         int salary = 60;
         if (salary > 50) {
             System.out.println("You are rich!");
+        }
+
+        int score = 5000;
+        if (score == 5000) {
+            System.out.println("You have got the high score");
+            System.out.println("This was executed");
         }
     }
 }
