9d79b5173f3dcaa12f145e0547a589a7845d303d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-20 19:14:19 +0530

    Create a method to calculateScore

--- Methods/Recap/src/com/company/MethodsExample.java
+++ Methods/Recap/src/com/company/MethodsExample.java
@@ -1,5 +1,15 @@
 package com.company;
 
 public class MethodsExample {
+    public static void main(String[] args) {
 
+    }
+    public void calculateScore(int score, boolean isAlive, int levelCompleted, int bonus) {
+        if (isAlive) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score is: " + finalScore);
+        } else {
+            System.out.println("Your final score is: " + score);
+        }
+    }
 }
