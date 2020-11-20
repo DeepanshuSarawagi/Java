91d924a20c4d81a7df589a89a0d08e6556e1bf73
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-20 19:18:07 +0530

    Test the code

--- Methods/Recap/src/com/company/MethodsExample.java
+++ Methods/Recap/src/com/company/MethodsExample.java
@@ -2,9 +2,9 @@ package com.company;
 
 public class MethodsExample {
     public static void main(String[] args) {
-
+        calculateScore(1000, true, 5, 200);
     }
-    public void calculateScore(int score, boolean isAlive, int levelCompleted, int bonus) {
+    public static void calculateScore(int score, boolean isAlive, int levelCompleted, int bonus) {
         if (isAlive) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score is: " + finalScore);
