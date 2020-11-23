4b2d9a2720159f047e772867f3c709d4a8c3b64a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-23 20:03:43 +0530

    remove redundant else block

--- Methods/Recap/src/com/company/methodChallenge.java
+++ Methods/Recap/src/com/company/methodChallenge.java
@@ -22,8 +22,7 @@ public class methodChallenge {
             return 2;
         } else if ((score >=100) && (score < 500)) {
             return 3;
-        } else {
-            return 4;
         }
+        return 4;
     }
 }
