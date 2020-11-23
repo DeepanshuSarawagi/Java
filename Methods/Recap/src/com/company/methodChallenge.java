a44255bd8fd2d0b8e04f465b481a446644c91b3e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-23 19:57:45 +0530

    Create second method calculateHighScorePosition()

--- Methods/Recap/src/com/company/methodChallenge.java
+++ Methods/Recap/src/com/company/methodChallenge.java
@@ -2,10 +2,25 @@ package com.company;
 
 public class methodChallenge {
     public static void main(String[] args) {
+//        int playerScore = calculateHighScorePosition(1500);
+//        displayHighScorePosition("Deepanshu", playerScore);
+//        playerScore = calculateHighScorePosition()
 
     }
 
     public static void displayHighScorePosition(String playerName, int position) {
         System.out.println(playerName + "managed to get position " + position + " in the hugh score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if ((score >= 500) && (score < 1000)) {
+            return 2;
+        } else if ((score >=100) && (score < 500)) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
