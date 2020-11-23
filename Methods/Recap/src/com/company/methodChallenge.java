ea752247e0402d7bdea4fdfe83a64371be3eaaa6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-23 19:54:19 +0530

    Create displayHighScorePosition() method to ouput the position

--- Methods/Recap/src/com/company/methodChallenge.java
+++ Methods/Recap/src/com/company/methodChallenge.java
@@ -4,4 +4,8 @@ public class methodChallenge {
     public static void main(String[] args) {
 
     }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + "managed to get position " + position + " in the hugh score table");
+    }
 }
