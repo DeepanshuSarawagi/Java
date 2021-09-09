af0bdc33dd0a51234c29ed4c47af6642cf568067
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-09 13:49:22 +0530

    Learn about variables in Java

---
+++ company-learning/Module2/Variables/src/com/deepanshu/VariablesExample/Main.java
@@ -0,0 +1,13 @@
+package com.deepanshu.VariablesExample;
+
+public class Main {
+    public static void main(String[] args) {
+        int myVar;
+        myVar = 50;
+        System.out.println("Value of myVar is " + myVar);
+        int anotherVar = 100;
+        System.out.println("Value of anotherVar is " + anotherVar);
+        myVar = anotherVar;
+        System.out.println("Value of myVar is " + myVar);
+    }
+}
