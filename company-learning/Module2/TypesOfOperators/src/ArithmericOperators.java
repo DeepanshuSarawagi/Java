3744f7095d04fb53e9a7837306060d4e9341bfef
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-09 17:55:38 +0530

    Artihmetic operators in Java

---
+++ company-learning/Module2/TypesOfOperators/src/ArithmericOperators.java
@@ -0,0 +1,15 @@
+public class ArithmericOperators {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
+        float a1 = 13f;
+        float b1 = 5f;
+        System.out.println("Division: " + (a1 / b1));
+        System.out.println("Modulo: " + (a1 % b1));
+    }
+}
