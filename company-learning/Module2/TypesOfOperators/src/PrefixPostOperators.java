4bbef6d20c1cd3c58fa92e2627311ab213f5dd6b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-09 18:03:18 +0530

    Postfix operation in Java

---
+++ company-learning/Module2/TypesOfOperators/src/PrefixPostOperators.java
@@ -0,0 +1,10 @@
+public class PrefixPostOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Prefix operation" + (++a));
+        System.out.println("Value of a is now " + a);
+        System.out.println("Postfix operation " + (a++));
+        System.out.println("Value of a is now " + a);
+
+    }
+}
