870d27f5f109f5a11f80ca7f5480b85228dd7c9e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-09 13:23:28 +0530

    Create new project to learn about packages in Java

---
+++ company-learning/Module1/PackagesDemo/src/com/deepanshu/getorganized/Main.java
@@ -0,0 +1,7 @@
+package com.deepanshu.getorganized;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! Get organized using packages in Java");
+    }
+}
