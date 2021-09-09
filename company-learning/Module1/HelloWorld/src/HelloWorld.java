20e9ff8ce5f6ee03daeb7adcf127b890bf196cfb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-09 13:11:24 +0530

    Line and document comments in Java

--- company-learning/Module1/HelloWorld/src/HelloWorld.java
+++ company-learning/Module1/HelloWorld/src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World for the 100th time in Java");
+        /*
+        This is a first line comment
+        This is a second line comment
+         */
+        System.out.println("Hello World for the 100th time in Java");  // This is line comment
+//        System.out.println("Hello World two");
+        System.out.println(/*This is a comment line and will not print*/"Hello World three");
     }
 }
