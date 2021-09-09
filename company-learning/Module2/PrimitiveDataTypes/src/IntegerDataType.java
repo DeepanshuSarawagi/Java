1e49f760a03a4fb8fb14ed80137349808b615778
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-09 14:03:43 +0530

    Learn about Primitive Data Types in Java

---
+++ company-learning/Module2/PrimitiveDataTypes/src/IntegerDataType.java
@@ -0,0 +1,15 @@
+public class IntegerDataType {
+    public static void main(String[] args) {
+        Integer myInt = Integer.MAX_VALUE;
+        System.out.println(myInt);
+        Long longInt = Long.MAX_VALUE;
+        System.out.println(longInt);
+        Byte byteVar = Byte.MAX_VALUE;
+        System.out.println(byteVar);
+        char regularU = 'U';
+        char unicodeChar = '\u00DA';
+        System.out.println(regularU);
+        System.out.println(unicodeChar);
+
+    }
+}
