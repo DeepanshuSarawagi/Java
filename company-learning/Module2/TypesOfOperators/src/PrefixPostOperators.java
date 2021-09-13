bf903a3fb9648619d1842809ed08e6bb8cad878d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-13 14:01:56 +0530

    Learn about operator precedence in Java

--- company-learning/Module2/TypesOfOperators/src/PrefixPostOperators.java
+++ company-learning/Module2/TypesOfOperators/src/PrefixPostOperators.java
@@ -25,3 +25,22 @@ class CompoundOperators{
         System.out.println("Result is now: " + result);
     }
 }
+
+class OperatorPrecedence{
+    public static void main(String[] args) {
+        operatorPrecedence();
+    }
+
+    private static void operatorPrecedence() {
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
+
+        int result = valA - valB / valC;
+        int result2 = (valA - valB) / valC;
+
+        System.out.println("Result1 is " + result);
+        System.out.println("Result2 is " + result2);
+    }
+}
