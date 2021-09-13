198f2fc34f96e349df1b1e2a3327ee65cfd938dd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-13 19:29:16 +0530

    conditional assignment in Java

--- company-learning/Module3/ConditionalLogic/src/ConditionalAssignment.java
+++ company-learning/Module3/ConditionalLogic/src/ConditionalAssignment.java
@@ -1,3 +1,5 @@
+import java.util.StringJoiner;
+
 public class ConditionalAssignment {
 
     public static void main(String[] args) {
@@ -5,5 +7,10 @@ public class ConditionalAssignment {
         int v2 = 5;
         int result = v1 > v2 ? v1 : v2;
         System.out.println(result);
+
+        float students = 75;
+        float rooms = 14;
+        float StudentsPerRoom = (students == 0.0f ? 0.0f : students / rooms);
+        System.out.println(StudentsPerRoom);
     }
 }
