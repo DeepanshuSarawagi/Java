f1f1509e1b864ef2e4e8cdea7a49dc2bba391095
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-13 15:16:46 +0530

    Learn about Type casting in Java

--- company-learning/Module2/TypeConversion/src/TypeConversionExample.java
+++ company-learning/Module2/TypeConversion/src/TypeConversionExample.java
@@ -4,7 +4,26 @@ public class TypeConversionExample {
         short SVal = 60;
         long lval = (long) SVal;
         System.out.println(lval);
-        short result = (short) lval;
-        System.out.println(result);
+        short result1 = (short) lval;
+        System.out.println(result1);
+
+        float floatVal = 3.14f;
+        double doubleVal = 4.07d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 1;
+
+        short result2 = (short) longVal;
+        result2 = (short) byteVal;
+        System.out.println(result2);
+
+        short result3 = (short) (byteVal - longVal);
+        System.out.println(result3);
+
+        float result4 = longVal - floatVal;
+        System.out.println(result4);
+
+        float result5 =  (float) (longVal - doubleVal);
+        System.out.println(result5);
     }
 }
