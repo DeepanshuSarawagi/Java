ed5aaf8019d60f2be3ad8e61549e30b769fcf10e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-15 19:57:45 +0530

    create constructors and getters in Passenger.java and Flight.java class

--- company-learning/Module5/ConstructorExample/src/Flight.java
+++ company-learning/Module5/ConstructorExample/src/Flight.java
@@ -1,2 +1,17 @@
 public class Flight {
+    private int seats;
+    private int passengers;
+
+    public Flight(int seats, int passengers) {
+        this.passengers = passengers;
+        this.seats = seats;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
 }
--- company-learning/Module5/ConstructorExample/src/Passenger.java
+++ company-learning/Module5/ConstructorExample/src/Passenger.java
@@ -1,2 +1,20 @@
 public class Passenger {
+    private int freeBags;
+    private int checkedInBags;
+
+    public Passenger(int freeBags) {
+        this.freeBags = freeBags;
+    }
+
+    public Passenger(int freeBags, int checkedInBags) {
+        this.checkedInBags = checkedInBags;
+    }
+
+    public int getFreeBags() {
+        return freeBags;
+    }
+
+    public int getCheckedInBags() {
+        return checkedInBags;
+    }
 }
