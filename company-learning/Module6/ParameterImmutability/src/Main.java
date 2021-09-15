1001b9dbdc2509d6b5f439a82ddc10901af6fe2a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-15 21:40:31 +0530

    Learn about Parameter Immutability in Java Class members

--- company-learning/Module6/ParameterImmutability/src/Main.java
+++ company-learning/Module6/ParameterImmutability/src/Main.java
@@ -13,6 +13,16 @@ public class Main {
         System.out.println("val2 is " + val2);
         System.out.println("Value of variables val1 and val2 is same. Since the changes to the variables passed as " +
                 "parameters in the above method is not changed outside of it. This is called as Parameter Immutability.");
+
+        System.out.println();
+
+        ParameterImmutability class1 = new ParameterImmutability(10);
+        ParameterImmutability class2 = new ParameterImmutability(20);
+        System.out.println("val1 is " + class1.getVal1() + " and val2 is " + class2.getVal1());
+        System.out.println();
+        swapClassMembers(class1, class2);
+        System.out.println();
+        System.out.println("val1 is " + class1.getVal1() + " and val2 is " + class2.getVal1());
     }
     protected static void swap (int i, int j) {
         int k = i;
@@ -21,5 +31,11 @@ public class Main {
         System.out.println("val1 is " + i);
         System.out.println("val2 is " + j);
         System.out.println();
+    }
+
+    protected static void swapClassMembers(ParameterImmutability i, ParameterImmutability j) {
+        int k = i.getVal1();
+        i.setVal1(j.getVal1());
+        j.setVal1(k);
     }
 }
---
+++ company-learning/Module6/ParameterImmutability/src/ParameterImmutability.java
@@ -0,0 +1,18 @@
+public class ParameterImmutability {
+    private int val1;
+
+
+    public ParameterImmutability(int val1) {
+        this.val1 = val1;
+    }
+
+    public int getVal1() {
+        return val1;
+    }
+
+
+    public void setVal1(int val1) {
+        this.val1 = val1;
+    }
+
+}
