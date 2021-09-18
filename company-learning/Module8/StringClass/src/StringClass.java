c33759506a8f93e09a1a89b55eb4cdf955cbbd7e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-18 19:35:39 +0530

    String class

---
+++ company-learning/Module8/StringClass/src/StringClass.java
@@ -0,0 +1,8 @@
+public class StringClass {
+    public static void main(String[] args) {
+        String greetings = "Hello! Prerana!";
+        String activity = " We are learning together on a weekend!";
+        System.out.println(greetings + activity);
+    }
+
+}
