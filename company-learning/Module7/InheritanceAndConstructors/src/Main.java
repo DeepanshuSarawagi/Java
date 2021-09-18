7425e42d00005f196e173ad0e4bb01d5684fea6a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-18 18:53:34 +0530

    Create new instances of class and call methods using instances

--- company-learning/Module7/InheritanceAndConstructors/src/Main.java
+++ company-learning/Module7/InheritanceAndConstructors/src/Main.java
@@ -3,5 +3,16 @@ public class Main {
         Flight F = new Flight(175);
         CargoFlight CF = new CargoFlight(580.45, 275);
 
+        CalculatorBase[] calculators = {
+                new Divider(100.0d, 25.0d),
+                new Subtracter(100.0d, 25.0d),
+                new Multiplier(100.0d, 25.0d),
+                new Adder(100.0d, 25.0d)
+        };
+
+        for(CalculatorBase calculator:calculators) {
+            calculator.calculate();
+            System.out.println("Result = " + calculator.getResult());
+        }
     }
 }
