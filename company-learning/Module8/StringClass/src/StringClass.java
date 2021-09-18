7efb3001ff41a1f41548684c10469b32d6ba12d5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-09-18 19:42:00 +0530

    Learn more about String type conversion

--- company-learning/Module8/StringClass/src/StringClass.java
+++ company-learning/Module8/StringClass/src/StringClass.java
@@ -3,6 +3,14 @@ public class StringClass {
         String greetings = "Hello! Prerana!";
         String activity = " We are learning together on a weekend!";
         System.out.println(greetings + activity);
+
+        /*
+         * Converting Non-String types into a String type
+         */
+
+        int iVal = 100;
+        String sVal = String.valueOf(iVal);
+        System.out.println(sVal);
     }
 
 }
